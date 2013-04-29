2689fa6fbb37a94ad11e15f806994a3df7e10a3c
automat <mail@finge.cc>
2013-04-29 17:12:10 +0200

    added doc + select events

--- src/controlKit/event/ckEventType.js
+++ src/controlKit/event/ckEventType.js
@@ -1,5 +1,13 @@
 var CKEventType =
 {
     VALUE_UPDATED : 'valueUpdated',
-    UPDATE_VALUE  : 'updateValue'
+    UPDATE_VALUE  : 'updateValue',
+
+    SELECT_TRIGGERED : 'selectTrigger',
+    TRIGGER_SELECT   : 'triggerSelect',
+
+    DOCUMENT_ON_MOUSE_DOWN : 'docOnMouseDown',
+    DOCUMENT_ON_MOUSE_UP   : 'docOnMouseUp',
+    DOCUMENT_ON_MOUSE_MOVE : 'docOnMouseMove',
+    DOCUMENT_ON_MOUSE_OUT  : 'docOnMouseOut'
 };
