89d9ac7864e05ee0102efa633b9a0f47cc80fd53
automat <mail@finge.cc>
2013-05-02 15:44:13 +0200

    added

---
+++ src/controlKit/core/ckDefaults.js
@@ -0,0 +1,14 @@
+var CKDefault =
+{
+    POSITION  : [20,20],
+    WIDTH     : 300,
+    WIDTH_MIN : 150,
+    WIDTH_MAX : 600,
+    RATIO     : 40,
+    LABEL     : 'Control Panel',
+
+    VALIGN : CKLayout.ALIGN_TOP,
+    ALIGN  : CKLayout.ALIGN_LEFT,
+
+    FIXED : true
+};
