56f586ae63a3a2897c3bd0d77df6453a77cc270a
automat <olafyoorsen@hotmail.com>
2013-05-14 18:51:39 +0200

    added scroll constants

--- src/controlKit/core/ckConstant.js
+++ src/controlKit/core/ckConstant.js
@@ -3,5 +3,11 @@ ControlKit.Constant =
     PADDING_WRAPPER : 12,
     PADDING_OPTIONS : 2,
     MIN_HEIGHT      : 25,
-    MIN_WIDTH       : 100
+    MIN_WIDTH       : 100,
+
+    PADDING_PRESET  : 20,
+
+    SCROLLBAR_BTN_HEIGHT : 17
+
+    //SCROLLBAR_
 };
