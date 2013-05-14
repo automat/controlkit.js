321b9c9cd7396463bfa712a8f1f0ddbe83797a90
automat <olafyoorsen@hotmail.com>
2013-05-14 18:53:03 +0200

    cleanup + added scroll + drag

--- src/controlKit/core/event/ckEventType.js
+++ src/controlKit/core/event/ckEventType.js
@@ -1,18 +1,30 @@
 ControlKit.EventType =
 {
-    VALUE_UPDATED      : 'valueUpdated',
-    UPDATE_VALUE       : 'updateValue',
+    VALUE_UPDATED          : 'valueUpdated',
+    UPDATE_VALUE           : 'updateValue',
 
-    SELECT_TRIGGERED   : 'selectTrigger',
-    TRIGGER_SELECT     : 'triggerSelect',
+    SELECT_TRIGGERED       : 'selectTrigger',
+    TRIGGER_SELECT         : 'triggerSelect',
 
-    PANEL_MOVE_BEGIN   : 'panelMoveBegin',
-    PANEL_MOVE         : 'panelMove',
-    PANEL_MOVE_END     : 'panelMoveEnd',
+    PANEL_MOVE_BEGIN       : 'panelMoveBegin',
+    PANEL_MOVE             : 'panelMove',
+    PANEL_MOVE_END         : 'panelMoveEnd',
 
-    SUBGROUP_HIDDEN    : 'subgroupHidden',
-    SUBGROUP_SHOWN     : 'subgroupShown',
+    SUBGROUP_HIDDEN        : 'subgroupHidden',
+    SUBGROUP_SHOWN         : 'subgroupShown',
 
-    INDEX_ORDER_CHANGED : 'indexOrderChanged',
-    CHANGE_INDEX_ORDER  : 'changeIndexOrder'
+    INDEX_ORDER_CHANGED    : 'indexOrderChanged',
+    CHANGE_INDEX_ORDER     : 'changeIndexOrder',
+
+    SCROLL_BEGIN           : 'scrollBegin',
+    SCROLL                 : 'scroll',
+    SCROLL_END             : 'scrollEnd',
+
+    INPUT_SELECTDRAG_START : 'inputSelectDragStart',
+    INPUT_SELECTDRAG       : 'inputSelectDrag',
+    INPUT_SELECTDRAG_END   : 'inputSelectDragEnd'
+
+
+
+
 };
