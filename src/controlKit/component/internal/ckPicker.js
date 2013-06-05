d33be4bb6576f3a8a0672508db101f11656f66e4
automat <olafyoorsen@hotmail.com>
2013-06-05 17:34:37 +0200

    adding picker functionality

--- src/controlKit/component/internal/ckPicker.js
+++ src/controlKit/component/internal/ckPicker.js
@@ -48,6 +48,8 @@ ControlKit.Picker = function()
 
     /*---------------------------------------------------------------------------------*/
 
+    this._colorMode = ControlKit.Picker.ColorModeType.HUE_MODE;
+
     this._drawPalette();
     this._drawSlider();
 
@@ -73,6 +75,9 @@ ControlKit.Picker.prototype =
             c.translateHalfFloat();
             c.fill(255);
             c.rect(0,0,width,height);
+
+
+
         }
         c.pop();
     },
@@ -81,19 +86,103 @@ ControlKit.Picker.prototype =
     {
         var c = this._sliderCanvas;
 
+        var width  = c.width,
+            height = c.height;
+
         c.clear();
+
+        var imageData = c.createImageData();
+
+        /*
         c.noStroke();
         c.push();
         {
             c.translateHalfFloat();
             c.fill(255);
-            //c.rect(0,0,width,height);
+            c.rect(0,0,width,height);
         }
         c.pop();
-    }
+        */
+    },
+
+    _HSV2RGB : function(hue,sat,val)
+    {
+        var max_hue = 360.0,
+            max_sat = 100.0,
+            max_val = 100.0;
+
+        var min_hue = 0.0,
+            min_sat = 0.0,
+            min_val = 0.0;
+
+        hue = hue % max_hue;
+        val = Math.max(min_val,Math.min(val,max_val))/max_val * 255.0;
+
+        if(sat <= min_sat){val = Math.round(val);return[val,val,val];}
+        else if(sat > max_sat)sat = max_sat;
+
+        sat = sat/max_sat;
+
+        //http://d.hatena.ne.jp/ja9/20100903/128350434
+
+        var hi = Math.floor(hue/60.0)% 6,
+            f  = (hue/60.0) - hi,
+            p  = val * (1 - sat),
+            q  = val * (1 - f * sat),
+            t  = val * (1 - (1 - f) * sat);
+
+        var r = 0,
+            g = 0,
+            b = 0;
+
+        switch(hi)
+        {
+            case 0:
+                r = val; g = t; b = p;
+                break;
+            case 1:
+                r = q; g = val; b = p;
+                break;
+            case 2:
+                r = p; g = val; b = t;
+                break;
+            case 3:
+                r = p; g = q; b = val;
+                break;
+            case 4:
+                r = t; g = p; b = val;
+                break;
+            case 5:
+                r = val; g = p; b = q;
+                break;
+            default:
+                break;
+        }
+
+        return [r,g,b];
+
+    },
+
+
+
+    setHue        : function(value) {},
+    setSaturation : function(value) {},
+    setValue      : function(value) {},
+
+    setR : function(value) {},
+    setG : function(value) {},
+    setB : function(value) {}
 };
 
 
 
 ControlKit.Picker.init        = function(){return ControlKit.Picker._instance = new ControlKit.Picker();};
 ControlKit.Picker.getInstance = function(){return ControlKit.Picker._instance;};
+
+ControlKit.Picker.ColorModeType =
+{
+    HUE_MODE        : 'hueMode',
+    SATURATION_MODE : 'saturationMode',
+    BRIGHTNESS_MODE : 'brightnessMode'
+
+}
