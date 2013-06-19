ac79e9ac2573735990e923998250643e17e786a8
automat <olafyoorsen@hotmail.com>
2013-06-19 16:20:21 +0200

    moved to core

---
+++ src/controlKit/core/ckLayout.js
@@ -0,0 +1,8 @@
+ControlKit.Layout =
+{
+    LEFT   : 'left',
+    RIGHT  : 'right',
+    TOP    : 'top',
+    BOTTOM : 'bottom',
+    NONE   : 'none'
+};
--- src/controlKit/core/layout/ckLayout.js
+++
@@ -1,7 +0,0 @@
-ControlKit.Layout =
-{
-    ALIGN_LEFT   : 'left',
-    ALIGN_RIGHT  : 'right',
-    ALIGN_TOP    : 'top',
-    ALIGN_BOTTOM : 'bottom'
-};
