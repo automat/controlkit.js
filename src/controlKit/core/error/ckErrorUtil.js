fa5f284dd89190e32cd92d2dec5bf7e034208106
automat <olafyoorsen@hotmail.com>
2013-06-23 10:27:05 +0200

    fix

--- src/controlKit/core/error/ckErrorUtil.js
+++ src/controlKit/core/error/ckErrorUtil.js
@@ -1,5 +1,5 @@
 ControlKit.ErrorUtil =
 {
     ReferenceError : function(object,key)       {return (object[key] === undefined);},
-    TypeError      : function(object,value,type){return Object.prototype.toString.call(object[value]) === Object.prototype.toString.call(Function);}
+    TypeError      : function(object,value,type){return Object.prototype.toString.call(object[value]) !== Object.prototype.toString.call(type);}
 };
