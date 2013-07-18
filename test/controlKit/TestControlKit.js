57e07ce22ec20386faf89ac773b044ea0cd14145
automat <olafyoorsen@hotmail.com>
2013-07-18 21:05:17 +0200

    + new components

--- test/controlKit/TestControlKit.js
+++ test/controlKit/TestControlKit.js
@@ -1,17 +1,22 @@
 
 var imports = [
-
-               //'style/style.js',
                'controlKit.js',
-
 
                'core/event/ckEvent.js',
                'core/event/ckEventDispatcher.js',
                'core/event/ckEventType.js',
 
-                'core/ckConstant.js',
-                'core/ckHistory.js',
-                'core/ckKit.js',
+               'core/color/ckColorMode.js',
+               'core/layout/ckLayoutMode.js',
+               'core/ckMetric.js',
+               'core/ckPreset.js',
+
+               'core/ckDefault.js',
+               'core/ckHistory.js',
+
+               'core/error/ckError.js',
+               'core/error/ckErrorUtil.js',
+               'core/ckKit.js',
 
                'core/dom/css/ckCSS.js',
 
@@ -22,22 +27,19 @@ var imports = [
                'core/dom/ckNodeType.js',
                'core/dom/ckNode.js',
 
-               //'core/dom/css/ckIcon.js',
-
-               'core/layout/ckLayout.js',
-
                'core/component/ckComponent.js',
                'core/component/ckObjectComponent.js',
-               'core/component/ckCanvas.js',
-               'core/component/ckCanvasComponent.js',
                'core/component/ckSVGComponent.js',
+
+               'core/color/ckColorUtil.js',
+
 
                'core/layout/ckScrollBar.js',
                'core/group/ckAbstractGroup.js',
                'core/group/ckGroup.js',
                'core/group/ckSubGroup.js',
 
-               'core/ckDefault.js',
+
                'core/ckPanel.js',
 
                'component/internal/ckOptions.js',
@@ -46,6 +48,7 @@ var imports = [
                'component/internal/ckPlotter.js',
                'component/internal/ckPresetBtn.js',
                'component/internal/ckOutput.js',
+               'component/internal/ckFunctionPlotType.js',
 
                'component/ckStringInput.js',
                'component/ckNumberInput.js',
@@ -64,19 +67,12 @@ var imports = [
                'component/ckStringOutput.js',
                'component/ckNumberOutput.js',
 
-               'component/ckConsole.js',
+               'component/internal/ckPicker.js',
 
-               'component/internal/ckPicker.js'
-
-
-
-
-
-
-
-
-
+               'component/ckCanvas.js',
+               'component/ckSVG.js'
               ];
+
 var i = -1,string;
 while(++i < imports.length)
 {
@@ -86,15 +82,18 @@ while(++i < imports.length)
 
 
 
+
+
 function TestControlKit(parentDomElementId)
 {
     var object = {string:'lorem ipsum',
+                  randomString : '',
                   stringPresets:['hy','well','bummm'],
                   number:26.0,
                   numberPresets:[10.0,20.0,345.0,12.0],
-                  range:[-1,1],
+                  range:[-10,10],
                   bool:true,
-                  slideValue:0.1,
+                  slideValue:0.0,
                   selectOptions:['hello','bello','cello'],
                   selectTarget:'hello',
                   func:function(x){return sin(x);},
@@ -114,6 +113,7 @@ function TestControlKit(parentDomElementId)
                       function(x){return stepInvCubed(sin(x))}],
                   funcTarget : null,
 
+
                   xyValue:[0.25,-0.35],
                   changeValue0:0.0,
                   changeValue1:0.0,
@@ -132,26 +132,208 @@ function TestControlKit(parentDomElementId)
         slideValue5:0.01,
         slideValue6:0.01,
         slideValue7:0.01,
+        implicitFunc : function(x,y)
+        {
+            var t = this.slideValue + this.funcs[0](x)*this.slideValue0,
+                v = this.slideValue1,
+                u = this.slideValue2;
+
+            return sin(v*abs(x)+t)*sin(u*abs(y)+t);
+        },
 
                    selectColorOptions : ['#5b3f95','#121212','#B32435'],
                    selectColorTarget  : null,
-                   color0 : '#5b3f95'
+                   color0 : '#5b3f95',
+                   colorArrInt : [255,255,255],
+                   colorArrIntPreset : [[0,0,0],[0,255,0],[0,0,255],[255,0,0]],
+                   colorArrFloat : [0.2,0.1225,0.5],
+                   colorArrFloatPreset : [[0,0.1,0],[0.25,0.1,0]]
                   };
 
     object.funcTarget = object.funcs[0];
     object.selectColorTarget = object.selectColorOptions[0];
 
-    var controlKit = new ControlKit.Kit(parentDomElementId,{trigger:true});
+    var controlKit = new ControlKit.Kit(parentDomElementId);
 
-    var panel0 = controlKit.addPanel({width: 400, align:'left', fixed: false, position:[20,20]}),
-        group0 = panel0.addGroup()
+    var panel0 = controlKit.addPanel({width:200,dock:{align:'left'}});
+
+        panel0.addGroup({label:'File IO'})
+            .addSubGroup({label:'Sub Settings'})
+            .addNumberInput(object,'number',{dp:4,presets:'numberPresets'})
+            .addStringInput(object,'string')
+            .addColor(   object, 'color0',       {presets:'selectColorOptions'})
+            .addColor(   object, 'colorArrInt',  {colorMode:'rgb',presets:'colorArrIntPreset'})
+            .addColor(   object, 'colorArrFloat',{colorMode:'rgbfv',presets:'colorArrFloatPreset'})
+            .addSelect(  object, 'selectOptions')
+            .addCheckbox(object, 'bool')
+            .addSlider(  object, 'slideValue', 'range')
+            .addRange(   object, 'range')
+            .addSlider(  object, 'slideValue0', 'range')
+            .addSlider(  object, 'slideValue1', 'range')
+            .addSlider(  object, 'slideValue2', 'range')
+            .addSubGroup({label:'Track Vals'})
+            .addValuePlotter(object, 'changeValue2', {height: 35, lineWidth: 2, lineColor: [237, 20, 91]})
+            .addValuePlotter(object, 'changeValue3', {height: 35, lineWidth: 2})
+            .addValuePlotter(object, 'changeValue2', {height: 35, lineColor: [237, 20, 91]})
+            .addValuePlotter(object, 'changeValue3', {height: 35})
+            .addValuePlotter(object, 'changeValue3', {height: 35})
+
+
+
+    var panel1 = controlKit.addPanel({width:150,dock:{align:'right'}});
+
+        panel1.addGroup()
+              .addSubGroup({useLabels:false})
+              .addFunctionPlotter(object,'funcTarget')
+              .addFunctionPlotter(object,'implicitFunc')
+            .addFunctionPlotter(object,'implicitFunc')
+            .addFunctionPlotter(object,'implicitFunc')
+            .addFunctionPlotter(object,'implicitFunc')
+             // .addStringOutput(object,'implicitFunc',{height:100});
+
+    panel0 = controlKit.addPanel({width:200,align:'left'});
+
+    panel0.addGroup({label:'File IO',align:'left'})
+        .addSubGroup({label:'Sub Settings'})
+        .addNumberInput(object,'number',{dp:4,presets:'numberPresets'})
+        .addStringInput(object,'string')
+        .addColor( object, 'color0',{presets:'selectColorOptions'})
+        .addColor(   object, 'colorArrInt',{colorMode:'rgb',presets:'colorArrIntPreset'})
+        .addColor(   object, 'colorArrFloat',{colorMode:'rgbfv',presets:'colorArrFloatPreset'})
+        .addSelect(  object, 'selectOptions')
+        .addCheckbox(object,'bool')
+        .addSlider(  object, 'slideValue', 'range')
+        .addRange(   object,'range')
+        .addSlider(  object,'slideValue0', 'range')
+        .addSlider(  object,'slideValue1', 'range')
+        .addSlider(  object,'slideValue2', 'range')
+
+
+
+
+
+
+
+    panel1 = controlKit.addPanel({width:50});
+    group0 = panel1.addGroup({label:'Button Bay'})
+        .addSubGroup({useLabels:false})
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+        .addButton('boom')
+
+
+    var panel1 = controlKit.addPanel({label:'non-fixed',
+                                      width: 200,
+                                      fixed: false,
+                                      align: 'left',
+                                      position:[100,100]}),
+        group0 = panel1.addGroup()
+            .addSubGroup({label:'Graph'})
+            .addSlider(  object, 'slideValue', 'range')
+            .addFunctionPlotter(object,'funcTarget',{label:'none'})
+            .addSelect(object,'funcs',{target:'funcTarget'})
+            //.addStringOutput(object,'funcTarget')
+           // .addStringOutput(object,'randomString',{wrap:true,height:100,label:'none'})
+           // .addStringOutput(object,'range',{wrap:true,height:100,label:'none'});
+
+    panel1 = controlKit.addPanel({label:'non-fixed right',
+                                      width: 200,
+                                      fixed: false,
+                                      align:'right',
+                                      position:[0,100]});
+        panel1.addGroup()
+            .addSubGroup({label:'Graph',enable:false})
+            .addSlider(  object, 'slideValue', 'range')
+            .addFunctionPlotter(object,'funcTarget',{height:100})
+            .addSubGroup()
+            .addPad(object,'xyChangeValue')
+            //.addStringOutput(object,'xyChangeValue',{height:50});
+
+
+
+
+    /*
+    var panel1 = controlKit.addPanel({width: 100,
+                                      align:'right',
+                                      position:[100,200],
+                                      fixed:false,
+                                      label:'Graph Control'}),
+        group0 = panel1.addGroup()
+            .addSubGroup({label:'Graph'})
+            .addSlider(  object, 'slideValue', 'range')
+            .addFunctionPlotter(object,'funcTarget',{label:'none'})
+            */
+
+
+
+
+    /*
+
+        panel0 = controlKit.addPanel({width: 200, dock:{align:'left'}});
+        panel0.addGroup()
+              .addSubGroup({label: 'File IO'})
+              .addButton('Load',null,{label:'none'})
+              .addButton('Save',null,{label:'none'})
+              .addButton('Save As',null,{label:'none'})
+            .addSubGroup({label: 'Color Setup'})
+              .addColor(object,'color0')
+              .addColor(object,'color0',{presets:'selectColorOptions'})
+              .addSelect(object,'selectColorOptions','selectColorTarget')
+              .addButton('Apply value')
+            .addSubGroup({label:'Misc'})
+              .addNumberInput(object,'number',{presets: 'numberPresets'})
+
+              .addFunctionPlotter(object, 'funcTarget', {label: 'Graph'})
+              .addStringOutput(object, 'funcTarget')
+              .addSelect(object, 'funcs', 'funcTarget')
+              .addSubGroup({label: 'Function Plot'})
+
+              .addRange(object, 'range')
+              .addSlider(object, 'range', 'slideValue');
+
+    panel0.addGroup()
+        .addSubGroup({label: 'File IO'})
+        .addButton('Load',null,{label:'none'})
+        .addButton('Save',null,{label:'none'})
+        .addButton('Save As',null,{label:'none'})
+        .addSubGroup({label: 'Color Setup'})
+        .addColor(object,'color0')
+        .addColor(object,'color0',{presets:'selectColorOptions'})
+        .addSelect(object,'selectColorOptions','selectColorTarget')
+        .addButton('Apply value')
+        .addSubGroup({label:'Misc'})
+        .addNumberInput(object,'number',{presets: 'numberPresets'})
+
+        .addFunctionPlotter(object, 'funcTarget', {label: 'Graph'})
+        .addStringOutput(object, 'funcTarget')
+        .addSelect(object, 'funcs', 'funcTarget')
+        .addSubGroup({label: 'Function Plot'})
+
+        .addRange(object, 'range')
+        .addSlider(object, 'range', 'slideValue');
+
+
+
+
+
+    var panel1 = controlKit.addPanel({width: 100, align:'left', fixed: false, position:[400,20]}),
+        group0 = panel1.addGroup()
             .addSubGroup({label:'Graph'})
             .addSelect(object,'funcs','funcTarget',{label:'Choose'})
             .addFunctionPlotter(object,'funcTarget',{label:'none'})
             .addStringOutput(object,'funcTarget',{label:'none'})
+
         ;
 
-    /*
+
     var panel0 = controlKit.addPanel({width: 200, align:'left', fixed: false, position:[20,20]}),
         group0 = panel0.addGroup()
                        .addSubGroup()
@@ -163,27 +345,21 @@ function TestControlKit(parentDomElementId)
             .addStringOutput(object,'funcTarget',{label:'none'})
             ;
 
-            */
 
 
-    /*
-    var panel0 = controlKit.addPanel({width: 200, align: 'left', fixed: false, position: [20, 20]});
-    var group01 = panel0.addGroup({height:300})
-        .addSubGroup({label: 'Function Select'})
-        .addColor(object,'color0')
-        .addColor(object,'color0',{presets:'selectColorOptions'})
-        .addSelect(object,'selectColorOptions','selectColorTarget')
-        .addButton('hello')
-        .addNumberInput(object,'number',{presets: 'numberPresets'})
 
 
-        .addFunctionPlotter(object, 'funcTarget', {label: 'Graph'})
-        .addStringOutput(object, 'funcTarget')
-        .addSelect(object, 'funcs', 'funcTarget')
-        .addSubGroup({label: 'Function Plot'})
-        //.addValuePlotter(object, 'changeValue0', {lineColor: [237, 20, 91]})
-        .addRange(object, 'range')
-        .addSlider(object, 'range', 'slideValue');
+
+
+    panel0.addGroup()
+        .addSubGroup()
+        .addSelect(object, 'selectOptions', 'selectTarget')
+        .addSelect(object, 'selectOptions', 'selectTarget')
+        .addSubGroup('subgroup 2')
+        .addRange(object, 'range' )
+        .addCheckbox(object, 'bool')
+        .addSubGroup('subgroup 3')
+        .addNumberOutput(object, 'xyValue', {wrap: true, height: 40, dp: 4})
 
 
 
@@ -276,33 +452,26 @@ function TestControlKit(parentDomElementId)
         .addSelect(object, 'selectOptions', 'selectTarget')
         .addButton('body go!')
         .addSubGroup()
-        .addRange(object, 'range', 'Range Comp')
-        .addCheckbox(object, 'bool', 'Bool Comp');
+        .addRange(object, 'range')
+        .addCheckbox(object, 'bool');
+
+    */
 
 
-    var control1 = controlKit.addPanel({width: 250, position: [205, 0], fixed: false});
-
-    control1.addGroup()
-        .addSubGroup()
-        .addSelect(object, 'selectOptions', 'selectTarget')
-        .addSelect(object, 'selectOptions', 'selectTarget')
-        .addSubGroup('subgroup 2')
-        .addRange(object, 'range' )
-        .addCheckbox(object, 'bool')
-        .addSubGroup('subgroup 3')
-        .addNumberOutput(object, 'xyValue', {wrap: true, height: 40, dp: 4});
 
 
-    var control2 = controlKit.addPanel({label: 'YAY Panel', width: 200, position: [460, 0], fixed: false});
 
-    control2.addGroup()
-        .addSubGroup({label: 'values1'})
-        .addValuePlotter(object, 'changeValue4', {height: 35, lineWidth: 2, lineColor: [237, 20, 91]})
-         .addSubGroup({label: 'values2'})
-        .addValuePlotter(object, 'changeValue4', {height: 35, lineWidth: 2, lineColor: [237, 20, 91]})
-        .addSubGroup({label: 'values3'})
-        .addValuePlotter(object, 'changeValue4', {height: 35, lineWidth: 2, lineColor: [237, 20, 91]})
 
+
+
+
+
+
+
+
+
+
+        /*
     */
 
     /*
@@ -404,10 +573,11 @@ function TestControlKit(parentDomElementId)
         var sint;
         function updateObject()
         {
-            object.output = randomString(2000);
+            object.output = randomString(1000);
 
             t+=object.slideValue;
 
+            /*
             object.changeValue0 = sin(t);//object.funcTarget(t);//sgn(sint);
             sint = Math.sin(t);
 
@@ -425,13 +595,16 @@ function TestControlKit(parentDomElementId)
             object.slideValue3 = sin(object.slideValue2+f);
             object.slideValue4 = sin(object.slideValue3+f);
             object.slideValue5 = sin(object.slideValue4+f);
+            */
 
-            /*
+
             t+=object.xyChangeValue[0];
             t+=randomFloat(-1,1)*object.xyChangeValue[1];
             sint = Math.sin(t);
 
             object.changeValue1 = tri(sint);
+
+            object.randomString = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata ';//randomString(200);
 
             object.changeValue2 = randomFloat(-1,1);
             object.changeValue3 = rect(sint);
@@ -439,15 +612,13 @@ function TestControlKit(parentDomElementId)
             object.changeValue5 = frac(sint);
             object.changeValue6 = sint;
             controlKit.update();
-            */
 
-            controlKit.update();
         }
 
         function loop(){requestAnimationFrame(loop);updateObject();}loop();
 
     function randomString(len, charSet) {
-        charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ';
         var randomString = '';
         for (var i = 0; i < len; i++) {
             var randomPoz = Math.floor(Math.random() * charSet.length);
