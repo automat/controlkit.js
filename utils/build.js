132a505098113a56ee927ba7c931d7e78e8e121e
Henryk Wollik <olafyoorsen@hotmail.com>
2014-11-01 00:24:44 +0100

    added build.js + placeholder

--- utils/build.js
+++ utils/build.js
@@ -1,23 +1,56 @@
 /**
  * build
- * Create a standalone version of controlKit with an optional custom style.
+ * Create a standalone version of controlKit with a custom style.
  */
 
-var browserify = require('browserify'),
-	controlKit = require('../src');
+var fs = require('fs');
 
 var args = process.argv.slice(2);
+var io   = args.indexOf('-o'),
+	is   = args.indexOf('-s');
 
-
-if(args.indexOf('-s') != -1){
-	console.log(args);
-	controlKit.Style.string = '';
+function esc(msg){
+	console.log(msg);
+	process.exit(1);
 }
 
-if(args.indexOf('-o') != -1){
-	
+if(args.length != 4){
+	esc('Wrong argl. Usage: build -s customStylePath -o outFileDir');
+} else if( io == -1){
+	esc('-o outFileDir argument missing.');
+} else if( is == -1){
+	esc('-s customStylePath argument missing');
 }
 
+var path = require('path'),
+	root = process.cwd();
 
+var through    = require('through'),
+	CleanCSS   = require('clean-css'),
+	browserify = require('browserify'),
+	UglifyJS   = require('uglify-js');
 
-process.exit(1);
+process.chdir(__dirname);
+
+var outFileDir       = path.join(root,args[io + 1]),
+	customStylePath  = path.join(root,args[is + 1]),
+	defaultStylePath = path.resolve('../lib/core/document/Style.js');
+
+var style = through(function(){
+						this.queue('var Style = { string : "' +
+								   new CleanCSS().minify(fs.readFileSync(customStylePath).toString()) +
+						           '" }; module.exports = Style;');
+					});
+
+browserify('../index.js')
+	.transform(function(file){
+		return file !== defaultStylePath ? through() : style;})
+	.bundle({
+		standalone:'ControlKit',
+		debug:true})
+	.pipe(fs.createWriteStream(
+		path.join(outFileDir,'controlKit.js')).on(
+		'close', function(){
+			fs.writeFileSync(path.join(outFileDir,'controlKit.min.js'),
+							 UglifyJS.minify(this.path).code);
+		}));
