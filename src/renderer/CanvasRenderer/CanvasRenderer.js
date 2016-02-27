98991a6a6d4f3f79c5553e32e62a8885f8dc1ed8
Henryk Wollik <olafyoorsen@hotmail.com>
2016-02-27 20:34:08 +0000

    CanvasRenderer added text support initial, move drawing to modules

--- src/renderer/CanvasRenderer/CanvasRenderer.js
+++ src/renderer/CanvasRenderer/CanvasRenderer.js
@@ -1,77 +1,11 @@
 import AbstractRenderer from "../AbstractRenderer";
 import NodeBase from "../DisplayNodeBase";
-import DisplayNode from "../DisplayNode";
+import Node from "../DisplayNode";
+import computeLayout_ from "css-layout";
+import {rect,roundedRect1,roundedRect4,line,lineH,lineV,fillTextMultiline} from "./DrawUtils";
+import {measureText} from "./MeasureUtils";
 
-function rect(ctx, x, y, width, height, inset = 0){
-    ctx.rect(x + inset * 0.5, y + inset * 0.5, width - inset, height - inset);
-}
-
-function roundedRect1( ctx, x, y, width, height, radius, inset  = 0){
-    x += inset * 0.5;
-    y += inset  * 0.5;
-    width  -= inset;
-    height -= inset;
-
-    radius = Math.min(Math.min(width,height) * 0.5, radius);
-
-    ctx.moveTo( x, y + radius );
-    ctx.lineTo( x, y + height - radius );
-    ctx.quadraticCurveTo( x, y + height, x + radius, y + height );
-    ctx.lineTo( x + width - radius, y + height) ;
-    ctx.quadraticCurveTo( x + width, y + height, x + width, y + height - radius );
-    ctx.lineTo( x + width, y + radius );
-    ctx.quadraticCurveTo( x + width, y, x + width - radius, y );
-    ctx.lineTo( x + radius, y );
-    ctx.quadraticCurveTo( x, y, x, y + radius );
-}
-
-function roundedRect4(ctx, x, y, width, height, tl, tr, br, bl, inset = 0){
-    x += inset * 0.5;
-    y += inset  * 0.5;
-    width  -= inset;
-    height -= inset;
-
-    let min = Math.min(width,height) * 0.5;
-    tl = Math.min(min,tl);
-    bl = Math.min(min,bl);
-    br = Math.min(min,br);
-    tr = Math.min(min,tr);
-
-    ctx.moveTo( x, y + tl );
-    ctx.lineTo( x, y + height - bl );
-    ctx.quadraticCurveTo( x, y + height, x + bl, y + height );
-    ctx.lineTo( x + width - br, y + height) ;
-    ctx.quadraticCurveTo( x + width, y + height, x + width, y + height - br );
-    ctx.lineTo( x + width, y + tr );
-    ctx.quadraticCurveTo( x + width, y, x + width - tr, y );
-    ctx.lineTo( x + tl, y );
-    ctx.quadraticCurveTo( x, y, x, y + tl );
-}
-
-function line(ctx, x0, y0, x1, y1){
-    ctx.moveTo(x0,y0);
-    ctx.lineTo(x1,y1);
-}
-
-function lines(ctx, lines){
-    for(let i = 0, l = lines.length; i < l; i+=4){
-        ctx.moveTo(lines[i  ],lines[i+1]);
-        ctx.lineTo(lines[i+2],lines[i+2])
-    }
-}
-
-function lineH(ctx, x0,x1,y){
-    ctx.moveTo(x0,y);
-    ctx.lineTo(x1,y);
-}
-
-function lineV(ctx, x,y0,y1){
-    ctx.moveTo(x,y0);
-    ctx.lineTo(x,y1);
-}
-
-
-export default class CanvasRenderer extends AbstractRenderer{
+export default class CanvasRenderer extends AbstractRenderer {
     constructor(canvas){
         super();
 
@@ -82,191 +16,237 @@ export default class CanvasRenderer extends AbstractRenderer{
         this._debugDraw = true;
     }
 
-    computeContentSize(node){
-        let ctx = this._ctx;
-
-        let style  = node.layoutNode.style;
-        let layout = node.layoutNode.layout;
-
-        let paddingTop;
-        let paddingRight;
-        let paddingBottom;
-        let paddingLeft;
-
-
-        if(style.padding){
-            paddingTop = paddingRight = paddingBottom = paddingLeft = style.padding;
-        } else {
-            paddingTop = style.paddingTop || 0;
-            paddingRight = style.paddingRight || 0;
-            paddingLeft = style.paddingLeft || 0;
-            paddingBottom = style.paddingBottom || 0
-        }
-
-        let borderWidth = style.borderWidth || 0;
-
-        if(node.textContent && node.textContent.length != 0){
-            let fontSize = style.fontSize;
-
-            ctx.font = `${fontSize}px Arial`;
-            let size   = ctx.measureText(node.textContent);
-
-            let height = layout.height - (paddingBottom + paddingTop + borderWidth * 2);
-            //console.log(fontSize,height);
-            if(fontSize > height){
-                node.style.height = 200;
-            }
-            node.layoutNode.shouldUpdate = true;
-        }
-
-        for(let child of node.children){
-            this.computeContentSize(child);
-        }
-    }
-
     computeLayout(){
-        this._base.computeLayout();
-        this.computeContentSize(this._base);
-        this._base.computeLayout();
+        let ctx = this._ctx;
+        this._base.updateLayoutNode();
+
+        function measure(maxWidth = 0, maxHeight = 0){
+            let layout = this._layoutNode.layout;
+            let style = this._layoutNode.style;
+
+            let borderWidth = style.borderWidth || 0;
+            let fontSize = style.fontSize || 0;
+            let paddingTop = style.paddingTop || 0;
+            let paddingRight = style.paddingRight || 0;
+            let paddingLeft = style.paddingLeft || 0;
+            let paddingBottom = style.paddingBottom || 0;
+
+            if(style.padding){
+                paddingTop = paddingRight = paddingBottom = paddingLeft = style.padding;
+            }
+
+            ctx.font = `${fontSize}px ${style.fontFamily || 'Arial'}`;
+
+            let width = 0;
+            let height = 0;
+
+            switch(style.whiteSpace){
+                case 'nowrap':
+                    width = ctx.measureText(this._textContent).width;
+                    height = fontSize;
+                    break;
+
+                case 'normal':
+                default:
+                    let size = measureText(
+                        ctx, this._textContent,{
+                            fontSize : fontSize,
+                            lineHeight : style.lineHeight,
+                            maxWidth : Math.max(layout.width, maxWidth) - (paddingRight + paddingLeft + borderWidth * 2)
+                        }
+                    );
+                    width = size.width;
+                    height = size.height;
+                    break;
+            }
+
+            return {
+                width : Math.max(width, maxWidth),
+                height : Math.max(height, maxHeight)
+            };
+        }
+
+        function inject(node){
+            let style = node.layoutNode.style;
+
+            if(node.textContent && node.textContent.length !== 0){
+                style.measure = measure.bind(node);
+            }else if(style.measure){
+                delete style.measure;
+            }
+
+            for(let child of node.children){
+                inject(child);
+            }
+        }
+
+        inject(this._base);
+        computeLayout_(this._base._layoutNode);
     }
 
     drawNodeDebug(node){
         let ctx = this._ctx;
 
         let layout = node.layoutNode.layout;
-        let style  = node.layoutNode.style;
+        let style = node.layoutNode.style;
 
-        function drawMargin(t,r=t,b=t,l=t){
+        function drawMargin(t, r = t, b = t, l = t){
             if(t === 0 || r === 0 || b === 0 || l === 0){
                 return;
             }
             let centerV = layout.height * 0.5;
             let centerH = layout.width * 0.5;
 
-            let handle   = Math.min(Math.min(layout.width,layout.height),20);
+            let handle = Math.min(Math.min(layout.width, layout.height), 20);
             let handleVA = centerV - handle * 0.5;
             let handleVB = centerV + handle * 0.5;
             let handleHA = centerH - handle * 0.5;
             let handleHB = centerH + handle * 0.5;
 
-            ctx.setLineDash([2,2]);
+            ctx.setLineDash([2, 2]);
             ctx.beginPath();
-            lineV(ctx,-l,handleVA, handleVB);
-            lineV(ctx,0,handleVA, handleVB);
-            lineH(ctx,-l,0,centerV);
-            lineV(ctx,layout.width + r, handleVA, handleVB);
-            lineV(ctx,layout.width, handleVA, handleVB);
-            lineH(ctx,layout.width, layout.width + r, centerV);
-            lineH(ctx,handleHA,handleHB,-t);
-            lineH(ctx,handleHA,handleHB,0);
-            lineV(ctx,centerH,-t,0);
-            lineH(ctx,handleHA,handleHB,layout.height);
-            lineH(ctx,handleHA,handleHB,layout.height + b);
-            lineV(ctx,centerH,layout.height,layout.height + b);
+            lineV(ctx, -l, handleVA, handleVB);
+            lineV(ctx, 0, handleVA, handleVB);
+            lineH(ctx, -l, 0, centerV);
+            lineV(ctx, layout.width + r, handleVA, handleVB);
+            lineV(ctx, layout.width, handleVA, handleVB);
+            lineH(ctx, layout.width, layout.width + r, centerV);
+            lineH(ctx, handleHA, handleHB, -t);
+            lineH(ctx, handleHA, handleHB, 0);
+            lineV(ctx, centerH, -t, 0);
+            lineH(ctx, handleHA, handleHB, layout.height);
+            lineH(ctx, handleHA, handleHB, layout.height + b);
+            lineV(ctx, centerH, layout.height, layout.height + b);
             ctx.stroke();
 
             ctx.strokeStyle = '#00ff00';
-            ctx.setLineDash([5,5]);
+            ctx.setLineDash([5, 5]);
             ctx.beginPath();
-            rect(ctx,-l,-t,layout.width+r+l,layout.height+b+t);
+            rect(ctx, -l, -t, layout.width + r + l, layout.height + b + t);
             ctx.stroke();
         }
 
         function drawSize(inset){
             ctx.beginPath();
             if(style.borderRadius){
-                roundedRect1(ctx,0,0,layout.width,layout.height,style.borderRadius,inset);
-            } else {
+                roundedRect1(ctx, 0, 0, layout.width, layout.height, style.borderRadius, inset);
+            }else{
                 let tl = style.borderRadiusTopLeft || 0;
                 let tr = style.borderRadiusTopRight || 0;
                 let br = style.borderRadiusBottomRight || 0;
                 let bl = style.borderRadiusBottomLeft || 0;
 
                 if(tl === 0 && tr === 0 && br === 0 && bl === 0){
-                    rect(ctx,0,0,layout.width,layout.height,inset);
-                } else {
-                    roundedRect4(ctx,0,0,layout.width,layout.height,tl,tr,br,bl,inset);
+                    rect(ctx, 0, 0, layout.width, layout.height, inset);
+                }else{
+                    roundedRect4(ctx, 0, 0, layout.width, layout.height, tl, tr, br, bl, inset);
                 }
             }
             ctx.stroke();
         }
 
         ctx.save();
-            ctx.translate(layout.left,layout.top);
+        ctx.translate(layout.left, layout.top);
 
-            let borderWidth = style.borderWidth || 0;
-            ctx.setLineDash([5,5]);
+        let borderWidth = style.borderWidth || 0;
+        ctx.setLineDash([5, 5]);
 
-            //padding all
-            ctx.strokeStyle = '#00ff00';
+        //padding all
+        ctx.strokeStyle = '#00ff00';
+        if(style.padding){
+            let padding = style.padding + borderWidth;
+            ctx.beginPath();
+            lineH(ctx, 0, layout.width, padding);
+            lineH(ctx, 0, layout.width, layout.height - padding);
+            lineV(ctx, padding, 0, layout.height);
+            lineV(ctx, layout.width - padding, 0, layout.height);
+            ctx.stroke();
+
+        //padding individual
+        }else{
+            ctx.beginPath();
+            if(style.paddingTop){
+                lineH(ctx, 0, layout.width, style.paddingTop + borderWidth);
+            }
+            if(style.paddingRight){
+                lineV(ctx, layout.width - borderWidth - style.paddingRight, 0, layout.height);
+            }
+            if(style.paddingBottom){
+                lineH(ctx, 0, layout.width, layout.height - style.paddingBottom - borderWidth);
+            }
+            if(style.paddingLeft){
+                lineV(ctx, style.paddingLeft + borderWidth, 0, layout.height);
+            }
+            ctx.stroke();
+        }
+
+        //margin all
+        ctx.strokeStyle = '#ff00ff';
+        if(style.margin){
+            drawMargin(style.margin);
+        //margin individual
+        }else{
+            drawMargin(style.marginTop || 0, style.marginRight || 0, style.marginBottom || 0, style.marginLeft || 0)
+        }
+
+        ctx.setLineDash([]);
+
+        ctx.strokeStyle = '#ffff00';
+        if(style.borderWidth){
+            ctx.lineWidth = style.borderWidth;
+            drawSize(style.borderWidth);
+            ctx.lineWidth = 1;
+        }
+
+        ctx.setLineDash([5, 5]);
+
+        //dimension
+        ctx.strokeStyle = '#ff0000';
+        drawSize();
+
+        //origin
+        ctx.fillStyle = '#0000ff';
+        ctx.fillRect(0, 0, 5, 5);
+
+        //text-content
+        if(node.textContent && node.textContent.length != 0){
+            let fontSize = style.fontSize || 0;
+            let paddingTop = style.paddingTop || 0;
+            let paddingRight = style.paddingRight || 0;
+            let paddingLeft = style.paddingLeft || 0;
+            let paddingBottom = style.paddingBottom || 0;
+
             if(style.padding){
-                let padding = style.padding + borderWidth;
-                ctx.beginPath();
-                lineH(ctx,0,layout.width,padding);
-                lineH(ctx,0,layout.width,layout.height - padding);
-                lineV(ctx,padding,0,layout.height);
-                lineV(ctx,layout.width - padding,0,layout.height);
-                ctx.stroke();
-            //padding individual
-            } else {
-                ctx.beginPath();
-                if(style.paddingTop){
-                    lineH(ctx,0,layout.width,style.paddingTop + borderWidth);
-                }
-                if(style.paddingRight){
-                    lineV(ctx,layout.width - borderWidth - style.paddingRight,0,layout.height);
-                }
-                if(style.paddingBottom){
-                    lineH(ctx,0,layout.width,layout.height - style.paddingBottom - borderWidth);
-                }
-                if(style.paddingLeft){
-                    lineV(ctx,style.paddingLeft + borderWidth,0,layout.height);
-                }
-                ctx.stroke();
+                paddingTop = paddingRight = paddingBottom = paddingLeft = style.padding;
             }
 
-            //margin all
-            ctx.strokeStyle = '#ff00ff';
-            if(style.margin){
-                drawMargin(style.margin);
-            } else {
-                drawMargin(style.marginTop || 0, style.marginRight || 0, style.marginBottom || 0,  style.marginLeft || 0)
+            let x = borderWidth + paddingLeft;
+            let y = borderWidth + paddingTop + fontSize - 2;
+
+            ctx.font = `${fontSize}px ${style.fontFamily || 'Arial'}`;
+
+            switch(style.whiteSpace){
+                case 'nowrap':
+                    ctx.fillText(node.textContent, x, y);
+                    break;
+                case 'normal':
+                default:
+                    fillTextMultiline(
+                        ctx, node.textContent,
+                        x, y, {
+                            fontSize: fontSize,
+                            lineHeight: style.lineHeight,
+                            textAlign : style.textAlign,
+                            maxWidth: layout.width - (paddingRight + paddingLeft + borderWidth * 2)
+                        }
+                    );
+                    break;
             }
+        }
 
-            ctx.setLineDash([]);
-
-            ctx.strokeStyle = '#00ff00';
-            if(style.borderWidth){
-                ctx.lineWidth = style.borderWidth;
-                drawSize(style.borderWidth);
-                ctx.lineWidth = 1;
-            }
-
-            ctx.setLineDash([5,5]);
-
-            //dimension
-            ctx.strokeStyle = '#ff0000';
-            drawSize();
-
-            //origin
-            ctx.fillStyle = '#0000ff';
-            ctx.fillRect(0,0,5,5);
-
-            //text-content
-            if(node.textContent && node.textContent.length != 0){
-                let fontSize = style.fontSize || 0;
-                let paddingTop = style.paddingTop || 0;
-                let paddingLeft = style.paddingLeft || 0;
-                if(style.padding){
-                    paddingTop = paddingLeft = style.padding;
-                }
-                ctx.font=`${fontSize}px Arial`;
-                ctx.fillText(node.textContent, borderWidth + paddingLeft, borderWidth + paddingTop + fontSize - 2)
-            }
-
-            for(let child of node.children){
-                this.drawNodeDebug(child);
-            }
+        for(let child of node.children){
+            this.drawNodeDebug(child);
+        }
         ctx.restore();
     }
 
@@ -275,7 +255,7 @@ export default class CanvasRenderer extends AbstractRenderer{
         let ctx = this._ctx;
 
         if(this._debugDraw){
-            ctx.clearRect(0,0,this._base.width, this._base.height);
+            ctx.clearRect(0, 0, this._base.width, this._base.height);
             //console.log(this._base);
             this.drawNodeDebug(this._base);
             return;
