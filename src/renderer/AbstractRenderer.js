075c10565dec8382611337de3e222cd9c527443c
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-06 00:27:55 +0100

    AbstractRenderer removed _debugDraw

--- src/renderer/AbstractRenderer.js
+++ src/renderer/AbstractRenderer.js
@@ -6,7 +6,6 @@ const STR_ERROR_NOT_IMPLEMENTED = 'Function not implemented.';
 export default class AbstractRenderer extends EventDispatcher{
     constructor(){
         super();
-        this._drawDebug = false;
         this._base = null;
     }
 
