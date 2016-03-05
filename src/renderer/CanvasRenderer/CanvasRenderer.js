b0edb601491896921c087581e85d8e6814fd51d2
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-05 23:46:17 +0100

    CanvasRenderer added debug draw layout, bounds, hover

--- src/renderer/CanvasRenderer/CanvasRenderer.js
+++ src/renderer/CanvasRenderer/CanvasRenderer.js
@@ -4,17 +4,67 @@ import Node from "../DisplayNode";
 import computeLayout_ from "css-layout";
 import {rect,roundedRect1,roundedRect4,line,lineH,lineV,fillTextMultiline} from "./DrawUtils";
 import {measureText} from "./MeasureUtils";
+import MouseEvent from "../../input/MouseEvent";
 
 export default class CanvasRenderer extends AbstractRenderer {
-    constructor(canvas){
+
+    constructor(canvas,options = {}){
+        options.debugDrawLayout = options.debugDrawLayout === undefined ? false : options.debugDrawLayout;
+        options.debugDrawBounds = options.debugDrawBounds === undefined ? false : options.debugDrawBounds;
+        options.debugDrawHover  = options.debugDrawHover  === undefined ? false : options.debugDrawHover;
+
         super();
 
         this._canvas = canvas;
         this._ctx = canvas.getContext('2d');
         this._base = new NodeBase();
         this._base.size = [canvas.width, canvas.height];
-        this._debugDraw = true;
+
+        this._debugDrawLayout = options.debugDrawLayout;
+        this._debugDrawBounds = options.debugDrawBounds;
+        this._debugDrawHover  = options.debugDrawHover;
+
+        // INPUT HANDLE
+        let self = this;
+        this._canvas.addEventListener('mousedown',function(e){
+            self._base.handleMouseDown({
+                x : e.pageX,
+                y : e.pageY,
+                button : e.button,
+                altKey : e.altKey,
+                ctrlKey : e.ctrlKey
+            });
+        });
+        this._canvas.addEventListener('mouseup',function(e){
+            self._base.handleMouseUp({
+                x : e.pageX,
+                y : e.pageY,
+                button : e.button,
+                altKey : e.altKey,
+                ctrlKey : e.ctrlKey
+            });
+        });
+        this._canvas.addEventListener('mousemove',function(e){
+            self._base.handleMouseMove({
+                x : e.pageX,
+                y : e.pageY,
+                button : e.button,
+                altKey : e.altKey,
+                ctrlKey : e.ctrlKey
+            });
+        });
+
+        if(this._debugDrawHover){
+            this._base.addEventListener(MouseEvent.MOUSE_MOVE,function(e){
+                self.draw();
+                self._debugDrawNodeHovered(e.data.node)
+            });
+        }
     }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+    // COMPUTE LAYOUT
+    /*----------------------------------------------------------------------------------------------------------------*/
 
     computeLayout(){
         let ctx = this._ctx;
@@ -84,11 +134,19 @@ export default class CanvasRenderer extends AbstractRenderer {
         computeLayout_(this._base._layoutNode);
     }
 
-    drawNodeDebug(node){
+    /*----------------------------------------------------------------------------------------------------------------*/
+    // DEBUG DRAW
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    _debugDrawNodeLayout(node){
         let ctx = this._ctx;
 
         let layout = node.layoutNode.layout;
-        let style = node.layoutNode.style;
+        let style  = node.layoutNode.style;
+
+        if(style.display === 'none' || style.visibility === 'hidden'){
+            return;
+        }
 
         function drawMargin(t, r = t, b = t, l = t){
             if(t === 0 || r === 0 || b === 0 || l === 0){
@@ -119,7 +177,7 @@ export default class CanvasRenderer extends AbstractRenderer {
             lineV(ctx, centerH, layout.height, layout.height + b);
             ctx.stroke();
 
-            ctx.strokeStyle = '#00ff00';
+            ctx.strokeStyle = 'rgb(200,200,200)';
             ctx.setLineDash([5, 5]);
             ctx.beginPath();
             rect(ctx, -l, -t, layout.width + r + l, layout.height + b + t);
@@ -148,11 +206,15 @@ export default class CanvasRenderer extends AbstractRenderer {
         ctx.save();
         ctx.translate(layout.left, layout.top);
 
+        ctx.fillStyle = '#ffffff';
+        drawSize();
+        ctx.fill();
+
         let borderWidth = style.borderWidth || 0;
         ctx.setLineDash([5, 5]);
 
         //padding all
-        ctx.strokeStyle = '#00ff00';
+        ctx.strokeStyle = 'rgb(200,200,200)';
         if(style.padding){
             let padding = style.padding + borderWidth;
             ctx.beginPath();
@@ -161,7 +223,6 @@ export default class CanvasRenderer extends AbstractRenderer {
             lineV(ctx, padding, 0, layout.height);
             lineV(ctx, layout.width - padding, 0, layout.height);
             ctx.stroke();
-
         //padding individual
         }else{
             ctx.beginPath();
@@ -198,11 +259,13 @@ export default class CanvasRenderer extends AbstractRenderer {
             ctx.lineWidth = 1;
         }
 
-        ctx.setLineDash([5, 5]);
-
         //dimension
         ctx.strokeStyle = '#ff0000';
         drawSize();
+
+        if(style.overflow === 'hidden'){
+            ctx.clip();
+        }
 
         //origin
         ctx.fillStyle = '#0000ff';
@@ -245,19 +308,70 @@ export default class CanvasRenderer extends AbstractRenderer {
         }
 
         for(let child of node.children){
-            this.drawNodeDebug(child);
+            this._debugDrawNodeLayout(child);
         }
         ctx.restore();
     }
+
+    _debugDrawNodeBounds(node){
+        let ctx = this._ctx;
+        let bounds = node.boundsGlobal;
+
+        if(bounds){
+            let x = bounds.x0;
+            let y = bounds.y0;
+            let w = bounds.x1 - x;
+            let h = bounds.y1 - y;
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = '#ff0000';
+            ctx.strokeRect(x,y,w,h);
+        }
+
+        for(let child of node.children){
+            this._debugDrawNodeBounds(child);
+        }
+    }
+
+    _debugDrawNodeHovered(node){
+        let ctx = this._ctx;
+        let bounds = node.boundsGlobal;
+
+        ctx.save();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#ff0000';
+        ctx.fillStyle = 'rgba(255,0,0,0.35)';
+        ctx.rect(
+            bounds.x0,bounds.y0,
+            bounds.x1 - bounds.x0,bounds.y1 - bounds.y0
+        );
+        ctx.fill();
+        ctx.stroke();
+        ctx.restore();
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+    // DRAW
+    /*----------------------------------------------------------------------------------------------------------------*/
 
     draw(){
         this.computeLayout();
         let ctx = this._ctx;
 
-        if(this._debugDraw){
+        if(this._debugDrawLayout || this._debugDrawBounds){
             ctx.clearRect(0, 0, this._base.width, this._base.height);
-            this.drawNodeDebug(this._base);
-            return;
+            ctx.save();
+            if(this._debugDrawLayout){
+                this._debugDrawNodeLayout(this._base);
+            }
+            ctx.restore();
+            ctx.save();
+            if(this._debugDrawBounds){
+                this._debugDrawNodeBounds(this._base);
+            }
+            ctx.restore();
+            if(this._debugDrawLayout){
+                return;
+            }
         }
     }
 }
