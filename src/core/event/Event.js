98561ce2920ba0001280c52dded74e2bb2268c70
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-05 23:44:02 +0100

    Event => es6

--- src/core/event/Event.js
+++ src/core/event/Event.js
@@ -1,35 +1,25 @@
-function Event(type, data){
-    this._sender = null;
-    this._type   = type;
-    this._data   = data;
+export default class Event{
+    constructor(type, data = null){
+        this._sender = null;
+        this._type   = type;
+        this._data   = data;
 
-    for(var prop in data) {
-        this[prop] = data[prop];
+        this._stopPropagation = false;
     }
 
-    this._stopPropagation = false;
+    stopPropagation(){
+        this._stopPropagation = true;
+    }
+
+    get sender(){
+        return this._sender;
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    get data(){
+        return this._data;
+    }
 }
-
-Event.prototype.copy = function(){
-    var evt = new Event(this._type, this._data);
-    evt.setSender(this._sender);
-    return evt;
-};
-
-Event.prototype.stopPropagation = function(){
-    this._stopPropagation = true;
-};
-
-Event.prototype.getSender = function(){
-    return this._sender;
-};
-
-Event.prototype.setSender = function(sender) {
-    this._sender = sender;
-};
-
-Event.prototype.getType = function(){
-    return this._type;
-};
-
-module.exports = Event;
