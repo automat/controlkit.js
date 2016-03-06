62fb6f81ee9248483e905ff493d6a92ec414cc5c
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-06 22:56:37 +0100

    KeyboardEvent initial

---
+++ src/input/KeyboardEvent.js
@@ -0,0 +1,16 @@
+import Event from "../core/event/Event";
+
+export default class KeyboardEvent extends Event{
+    constructor(type,data){
+        super(type,data);
+    }
+}
+
+KeyboardEvent.KEY_DOWN = 'keydown';
+KeyboardEvent.KEY_PRESS = 'keypress';
+KeyboardEvent.KEY_UP = 'keyup';
+
+KeyboardEvent.KEY_SPACE = 32;
+KeyboardEvent.KEY_ENTER = 13;
+KeyboardEvent.KEY_BACKSPACE = 8;
+KeyboardEvent.KEY_DELETE = 46;
