136439ce429251ce28ce204d57e0457f4d0cceea
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-06 22:57:40 +0100

    NodeType added INPUT_{TEXT,NUMBER}

--- src/renderer/NodeType.js
+++ src/renderer/NodeType.js
@@ -2,7 +2,8 @@ var NodeType = {
     BASE       : 'node-type-base',
     CONTAINER  : 'node-type-container',
     TEXT       : 'node-type-text',
-    INPUT_TEXT : 'node-type-input-text'
+    INPUT_TEXT : 'node-type-input-text',
+    INPUT_NUMBER : 'node-type-input-number'
 };
 
 export default NodeType;
