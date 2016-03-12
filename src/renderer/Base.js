a32cc582af1cb8c4bacf2b7a1bf8a083a35c2ab2
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-12 21:02:53 +0100

    Base added create{Nodes,NodeFromDetail,NodesFromDetail}

--- src/renderer/Base.js
+++ src/renderer/Base.js
@@ -5,6 +5,15 @@ var Base = {
     },
     createNode : function(type){
         return this._base.createNode(type);
+    },
+    createNodes : function(types){
+        return this._base.createNodes(types);
+    },
+    createNodeFromDetail : function(detail){
+        return this._base.createNodeFromDetail(detail);
+    },
+    createNodesFromDetail : function(details){
+        return this._base.createNodesFromDetail(details);
     }
 };
 
