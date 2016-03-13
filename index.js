caeec2f2439a4fbe83b42154eb3e379a5446d12b
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-14 00:09:17 +0100

    index.js fixed

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-var ControlKit        = require('./lib/ControlKit');
-	ControlKit.Canvas = require('./lib/component/Canvas');
-	ControlKit.SVG    = require('./lib/component/SVG');
-
-module.exports = ControlKit;
+import ControlKit from "./src/ControlKit";
+export default ControlKit;
