2c5a1a9bb9f1794cb4eca75baa4de7e793b0471f
Henryk Wollik <olafyoorsen@hotmail.com>
2016-09-16 17:05:38 +0200

    src component ComponentPreset initial

---
+++ src/component/ComponentPreset.js
@@ -0,0 +1,55 @@
+import EventEmitter from 'events';
+import createHtml from '../util/createHtml';
+import ComponentOptions from './ComponentOptions';
+
+const templatePresetWrap = `<div class="input-wrap-preset"></div>`;
+const templatePresetButton = `<button class="btn-preset"></button>`;
+
+export default class ComponentPreset extends EventEmitter{
+    constructor(input){
+        super();
+        this._options = null;
+
+        //elements
+        this._parent = input.parentNode;
+        this._input = input;
+        this._elementWrap = createHtml(templatePresetWrap);
+        this._elementButton = createHtml(templatePresetButton);
+
+        //listener
+        const options = ComponentOptions.sharedOptions();
+        this._elementButton.addEventListener('click',()=>{
+            options.dock(this._input);
+            options.options = this._options;
+            options.enable = true;
+            // this._elementButton.classList.add()
+        });
+        options.on('change',(info)=>{
+            if(info.input !== this._input){
+                return;
+            }
+            this.emit('change',info.value);
+        });
+    }
+
+    set options(value){
+        if(value){
+            this._parent.removeChild(this._input);
+            this._parent.appendChild(this._elementWrap);
+            this._parent.appendChild(this._elementButton);
+            this._elementWrap.appendChild(this._input);
+            this._options = value.slice(0);
+        } else {
+            if(this._parent.contains(this._elementWrap)){
+                this._parent.removeChild(this._elementWrap);
+                this._parent.removeChild(this._elementButton);
+                this._parent.appendChild(this._input);
+            }
+            this._options = null;
+        }
+    }
+
+    get options(){
+        return this._options ? this._options.slice(0) : null;
+    }
+}
