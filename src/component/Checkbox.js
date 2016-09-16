a380b31a56909346a3ed1083ed3f33a941ee80f6
Henryk Wollik <olafyoorsen@hotmail.com>
2016-09-16 17:05:54 +0200

    src component Checkbox initial

---
+++ src/component/Checkbox.js
@@ -0,0 +1,53 @@
+import createHtml from '../util/createHtml';
+import validateOption from 'validate-option';
+
+import ObjectComponent from './ObjectComponent';
+
+const template =
+    `<div class="input-wrap">
+        <input type="checkbox">
+     </div>`;
+
+export const DefaultConfig = Object.freeze({
+    label : null,
+    onChange : function(){},
+    annotation : null
+});
+
+export default class Checkbox extends ObjectComponent{
+    /**
+     * @param {SubGroup} parent - The parent sub-group
+     * @param {Object} object - The target object
+     * @param {String} key - The target object property key to mapped
+     * @param {Object} config - The component configuration
+     */
+    constructor(parent,object,key,config){
+        config = validateOption(config,DefaultConfig);
+        config.label = config.label == null ? key : config.label;
+
+        super(parent,object,key,{
+            label : config.label,
+            annotation:config.annotation,
+            onChange : config.onChange
+        });
+
+        //elements
+        this._element.classList.add('type-input');
+        this._element.appendChild(createHtml(template));
+        this._elementInput = this._element.querySelector('input');
+        this._elementInput.addEventListener('input',()=>{
+            this.value = this._elementInput.checked;
+        });
+
+        //sync
+        this.sync();
+    }
+
+    /**
+     * Forces the component to sync with its underlying property e.g. in case it
+     * got changed externally.
+     */
+    sync(){
+        this._elementInput.checked = this.value;
+    }
+}
