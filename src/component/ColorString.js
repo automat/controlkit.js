43a5c1e9f6b5db1a06c6fb1f98292432c6e84667
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-03 01:20:22 +0200

    src component ColorString initial

---
+++ src/component/ColorString.js
@@ -0,0 +1,297 @@
+export const HexAliceBlue = '#F0F8FF';
+export const HexAntiqueWhite = '#FAEBD7';
+export const HexAqua = '#00FFFF';
+export const HexAquamarine = '#7FFFD4';
+export const HexAzure = '#F0FFFF';
+export const HexBeige = '#F5F5DC';
+export const HexBisque = '#FFE4C4';
+export const HexBlack = '#000000';
+export const HexBlanchedAlmond = '#FFEBCD';
+export const HexBlue = '#0000FF';
+export const HexBlueViolet = '#8A2BE2';
+export const HexBrown = '#A52A2A';
+export const HexBurlyWood = '#DEB887';
+export const HexCadetBlue = '#5F9EA0';
+export const HexChartreuse = '#7FFF00';
+export const HexChocolate = '#D2691E';
+export const HexCoral = '#FF7F50';
+export const HexCornflowerBlue = '#6495ED';
+export const HexCornsilk = '#FFF8DC';
+export const HexCrimson = '#DC143C';
+export const HexCyan = '#00FFFF';
+export const HexDarkBlue = '#00008B';
+export const HexDarkCyan = '#008B8B';
+export const HexDarkGoldenRod = '#B8860B';
+export const HexDarkGray = '#A9A9A9';
+export const HexDarkGrey = '#A9A9A9';
+export const HexDarkGreen = '#006400';
+export const HexDarkKhaki = '#BDB76B';
+export const HexDarkMagenta = '#8B008B';
+export const HexDarkOliveGreen = '#556B2F';
+export const HexDarkorange = '#FF8C00';
+export const HexDarkOrchid = '#9932CC';
+export const HexDarkRed = '#8B0000';
+export const HexDarkSalmon = '#E9967A';
+export const HexDarkSeaGreen = '#8FBC8F';
+export const HexDarkSlateBlue = '#483D8B';
+export const HexDarkSlateGray = '#2F4F4F';
+export const HexDarkSlateGrey = '#2F4F4F';
+export const HexDarkTurquoise = '#00CED1';
+export const HexDarkViolet = '#9400D3';
+export const HexDeepPink = '#FF1493';
+export const HexDeepSkyBlue = '#00BFFF';
+export const HexDimGray = '#696969';
+export const HexDimGrey = '#696969';
+export const HexDodgerBlue = '#1E90FF';
+export const HexFireBrick = '#B22222';
+export const HexFloralWhite = '#FFFAF0';
+export const HexForestGreen = '#228B22';
+export const HexFuchsia = '#FF00FF';
+export const HexGainsboro = '#DCDCDC';
+export const HexGhostWhite = '#F8F8FF';
+export const HexGold = '#FFD700';
+export const HexGoldenRod = '#DAA520';
+export const HexGray = '#808080';
+export const HexGrey = '#808080';
+export const HexGreen = '#008000';
+export const HexGreenYellow = '#ADFF2F';
+export const HexHoneyDew = '#F0FFF0';
+export const HexHotPink = '#FF69B4';
+export const HexIndianRed = '#CD5C5C';
+export const HexIndigo = '#4B0082';
+export const HexIvory = '#FFFFF0';
+export const HexKhaki = '#F0E68C';
+export const HexLavender = '#E6E6FA';
+export const HexLavenderBlush = '#FFF0F5';
+export const HexLawnGreen = '#7CFC00';
+export const HexLemonChiffon = '#FFFACD';
+export const HexLightBlue = '#ADD8E6';
+export const HexLightCoral = '#F08080';
+export const HexLightCyan = '#E0FFFF';
+export const HexLightGoldenRodYellow = '#FAFAD2';
+export const HexLightGray = '#D3D3D3';
+export const HexLightGrey = '#D3D3D3';
+export const HexLightGreen = '#90EE90';
+export const HexLightPink = '#FFB6C1';
+export const HexLightSalmon = '#FFA07A';
+export const HexLightSeaGreen = '#20B2AA';
+export const HexLightSkyBlue = '#87CEFA';
+export const HexLightSlateGray = '#778899';
+export const HexLightSlateGrey = '#778899';
+export const HexLightSteelBlue = '#B0C4DE';
+export const HexLightYellow = '#FFFFE0';
+export const HexLime = '#00FF00';
+export const HexLimeGreen = '#32CD32';
+export const HexLinen = '#FAF0E6';
+export const HexMagenta = '#FF00FF';
+export const HexMaroon = '#800000';
+export const HexMediumAquaMarine = '#66CDAA';
+export const HexMediumBlue = '#0000CD';
+export const HexMediumOrchid = '#BA55D3';
+export const HexMediumPurple = '#9370D8';
+export const HexMediumSeaGreen = '#3CB371';
+export const HexMediumSlateBlue = '#7B68EE';
+export const HexMediumSpringGreen = '#00FA9A';
+export const HexMediumTurquoise = '#48D1CC';
+export const HexMediumVioletRed = '#C71585';
+export const HexMidnightBlue = '#191970';
+export const HexMintCream = '#F5FFFA';
+export const HexMistyRose = '#FFE4E1';
+export const HexMoccasin = '#FFE4B5';
+export const HexNavajoWhite = '#FFDEAD';
+export const HexNavy = '#000080';
+export const HexOldLace = '#FDF5E6';
+export const HexOlive = '#808000';
+export const HexOliveDrab = '#6B8E23';
+export const HexOrange = '#FFA500';
+export const HexOrangeRed = '#FF4500';
+export const HexOrchid = '#DA70D6';
+export const HexPaleGoldenRod = '#EEE8AA';
+export const HexPaleGreen = '#98FB98';
+export const HexPaleTurquoise = '#AFEEEE';
+export const HexPaleVioletRed = '#D87093';
+export const HexPapayaWhip = '#FFEFD5';
+export const HexPeachPuff = '#FFDAB9';
+export const HexPeru = '#CD853F';
+export const HexPink = '#FFC0CB';
+export const HexPlum = '#DDA0DD';
+export const HexPowderBlue = '#B0E0E6';
+export const HexPurple = '#800080';
+export const HexRed = '#FF0000';
+export const HexRosyBrown = '#BC8F8F';
+export const HexRoyalBlue = '#4169E1';
+export const HexSaddleBrown = '#8B4513';
+export const HexSalmon = '#FA8072';
+export const HexSandyBrown = '#F4A460';
+export const HexSeaGreen = '#2E8B57';
+export const HexSeaShell = '#FFF5EE';
+export const HexSienna = '#A0522D';
+export const HexSilver = '#C0C0C0';
+export const HexSkyBlue = '#87CEEB';
+export const HexSlateBlue = '#6A5ACD';
+export const HexSlateGray = '#708090';
+export const HexSlateGrey = '#708090';
+export const HexSnow = '#FFFAFA';
+export const HexSpringGreen = '#00FF7F';
+export const HexSteelBlue = '#4682B4';
+export const HexTan = '#D2B48C';
+export const HexTeal = '#008080';
+export const HexThistle = '#D8BFD8';
+export const HexTomato = '#FF6347';
+export const HexTurquoise = '#40E0D0';
+export const HexViolet = '#EE82EE';
+export const HexWheat = '#F5DEB3';
+export const HexWhite = '#FFFFFF';
+export const HexWhiteSmoke = '#F5F5F5';
+export const HexYellow = '#FFFF00';
+export const HexYellowGreen = '#9ACD32';
+
+export const CSSColorStringMap = {
+    AliceBlue : HexAliceBlue,
+    AntiqueWhite : HexAntiqueWhite,
+    Aqua : HexAqua,
+    Aquamarine : HexAquamarine,
+    Azure : HexAzure,
+    Beige : HexBeige,
+    Bisque : HexBisque,
+    Black : HexBlack,
+    BlanchedAlmond : HexBlanchedAlmond,
+    Blue : HexBlue,
+    BlueViolet : HexBlueViolet,
+    Brown : HexBrown,
+    BurlyWood : HexBurlyWood,
+    CadetBlue : HexCadetBlue,
+    Chartreuse : HexChartreuse,
+    Chocolate : HexChocolate,
+    Coral : HexCoral,
+    CornflowerBlue : HexCornflowerBlue,
+    Cornsilk : HexCornsilk,
+    Crimson : HexCrimson,
+    Cyan : HexCyan,
+    DarkBlue : HexDarkBlue,
+    DarkCyan : HexDarkCyan,
+    DarkGoldenRod : HexDarkGoldenRod,
+    DarkGray : HexDarkGray,
+    DarkGrey : HexDarkGrey,
+    DarkGreen : HexDarkGreen,
+    DarkKhaki : HexDarkKhaki,
+    DarkMagenta : HexDarkMagenta,
+    DarkOliveGreen : HexDarkOliveGreen,
+    Darkorange : HexDarkorange,
+    DarkOrchid : HexDarkOrchid,
+    DarkRed : HexDarkRed,
+    DarkSalmon : HexDarkSalmon,
+    DarkSeaGreen : HexDarkSeaGreen,
+    DarkSlateBlue : HexDarkSlateBlue,
+    DarkSlateGray : HexDarkSlateGray,
+    DarkSlateGrey : HexDarkSlateGrey,
+    DarkTurquoise : HexDarkTurquoise,
+    DarkViolet : HexDarkViolet,
+    DeepPink : HexDeepPink,
+    DeepSkyBlue : HexDeepSkyBlue,
+    DimGray : HexDimGray,
+    DimGrey : HexDimGrey,
+    DodgerBlue : HexDodgerBlue,
+    FireBrick : HexFireBrick,
+    FloralWhite : HexFloralWhite,
+    ForestGreen : HexForestGreen,
+    Fuchsia : HexFuchsia,
+    Gainsboro : HexGainsboro,
+    GhostWhite : HexGhostWhite,
+    Gold : HexGold,
+    GoldenRod : HexGoldenRod,
+    Gray : HexGray,
+    Grey : HexGrey,
+    Green : HexGreen,
+    GreenYellow : HexGreenYellow,
+    HoneyDew : HexHoneyDew,
+    HotPink : HexHotPink,
+    IndianRed : HexIndianRed,
+    Indigo : HexIndigo,
+    Ivory : HexIvory,
+    Khaki : HexKhaki,
+    Lavender : HexLavender,
+    LavenderBlush : HexLavenderBlush,
+    LawnGreen : HexLawnGreen,
+    LemonChiffon : HexLemonChiffon,
+    LightBlue : HexLightBlue,
+    LightCoral : HexLightCoral,
+    LightCyan : HexLightCyan,
+    LightGoldenRodYellow : HexLightGoldenRodYellow,
+    LightGray : HexLightGray,
+    LightGrey : HexLightGrey,
+    LightGreen : HexLightGreen,
+    LightPink : HexLightPink,
+    LightSalmon : HexLightSalmon,
+    LightSeaGreen : HexLightSeaGreen,
+    LightSkyBlue : HexLightSkyBlue,
+    LightSlateGray : HexLightSlateGray,
+    LightSlateGrey : HexLightSlateGrey,
+    LightSteelBlue : HexLightSteelBlue,
+    LightYellow : HexLightYellow,
+    Lime : HexLime,
+    LimeGreen : HexLimeGreen,
+    Linen : HexLinen,
+    Magenta : HexMagenta,
+    Maroon : HexMaroon,
+    MediumAquaMarine : HexMediumAquaMarine,
+    MediumBlue : HexMediumBlue,
+    MediumOrchid : HexMediumOrchid,
+    MediumPurple : HexMediumPurple,
+    MediumSeaGreen : HexMediumSeaGreen,
+    MediumSlateBlue : HexMediumSlateBlue,
+    MediumSpringGreen : HexMediumSpringGreen,
+    MediumTurquoise : HexMediumTurquoise,
+    MediumVioletRed : HexMediumVioletRed,
+    MidnightBlue : HexMidnightBlue,
+    MintCream : HexMintCream,
+    MistyRose : HexMistyRose,
+    Moccasin : HexMoccasin,
+    NavajoWhite : HexNavajoWhite,
+    Navy : HexNavy,
+    OldLace : HexOldLace,
+    Olive : HexOlive,
+    OliveDrab : HexOliveDrab,
+    Orange : HexOrange,
+    OrangeRed : HexOrangeRed,
+    Orchid : HexOrchid,
+    PaleGoldenRod : HexPaleGoldenRod,
+    PaleGreen : HexPaleGreen,
+    PaleTurquoise : HexPaleTurquoise,
+    PaleVioletRed : HexPaleVioletRed,
+    PapayaWhip : HexPapayaWhip,
+    PeachPuff : HexPeachPuff,
+    Peru : HexPeru,
+    Pink : HexPink,
+    Plum : HexPlum,
+    PowderBlue : HexPowderBlue,
+    Purple : HexPurple,
+    Red : HexRed,
+    RosyBrown : HexRosyBrown,
+    RoyalBlue : HexRoyalBlue,
+    SaddleBrown : HexSaddleBrown,
+    Salmon : HexSalmon,
+    SandyBrown : HexSandyBrown,
+    SeaGreen : HexSeaGreen,
+    SeaShell : HexSeaShell,
+    Sienna : HexSienna,
+    Silver : HexSilver,
+    SkyBlue : HexSkyBlue,
+    SlateBlue : HexSlateBlue,
+    SlateGray : HexSlateGray,
+    SlateGrey : HexSlateGrey,
+    Snow : HexSnow,
+    SpringGreen : HexSpringGreen,
+    SteelBlue : HexSteelBlue,
+    Tan : HexTan,
+    Teal : HexTeal,
+    Thistle : HexThistle,
+    Tomato : HexTomato,
+    Turquoise : HexTurquoise,
+    Violet : HexViolet,
+    Wheat : HexViolet,
+    White : HexWhite,
+    WhiteSmoke : HexWhiteSmoke,
+    Yellow : HexYellow,
+    YellowGreen : HexYellowGreen
+};
