5d99baf985abd4d60d2bd2ece210b80d38b72eff
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-09 20:15:06 +0200

    src ScrollContainer added auto-scroll position on height conflict, track drag hold, scrollTo, scrollToTop, scrollToBottom

--- src/group/ScrollContainer.js
+++ src/group/ScrollContainer.js
@@ -11,16 +11,15 @@ const template =
     </div>`;
 
 export default class ScrollContainer{
+    /**
+     * @constructor
+     * @param {HTMLElement} target
+     */
     constructor(target){
-        this._state = {
-            height : null,
-            scrollY : 0,
-            scrollYNorm : 0,
-            dragging : false,
-            handleHeight : 0,
-            handleOffset : 0,
-            handleDragging : false
-        };
+        this._height = null;
+        this._scrollY = 0;
+        this._trackDragging = false;
+        this._handleDragging = false;
 
         this._target = target;
         this._parent = this._target.parentNode;
@@ -29,77 +28,69 @@ export default class ScrollContainer{
         this._elementTrack = this._element.querySelector('.scroll-track');
         this._elementHandle = this._element.querySelector('.scroll-handle');
 
-        const getDelta = (e)=>{
-            return -((e.wheelDelta * 0.5) || (e.detail * -6.0));
-        };
-
         //scroll within container
         const onContainerScroll = (e)=>{
-            if(this._state.height === null){
+            if(this._height === null){
                 return;
             }
-            const delta = getDelta(e);
-
-            //update scroll container position
-            const height = this._element.offsetHeight;
-            const heightTarget = this._target.offsetHeight;
-            const min = 0;
-            const max = heightTarget - height;
-
-            this._state.scrollY += delta;
-            this._state.scrollY = Math.max(min,Math.min(this._state.scrollY,max));
-            this._state.scrollYNorm = this._state.scrollY / max;
-            this._target.style.marginTop = -this._state.scrollY + 'px';
-
-            //update scroll handle position
-            const heightHandle = this._elementHandle.offsetHeight;
-            const heightTrack = this._elementTrack.offsetHeight;
-            const ratio = heightHandle / heightTrack;
-
-            this._elementHandle.style.marginTop = this._state.scrollYNorm * (1.0 - ratio) * heightTrack + 'px';
-
+            this._scrollY += -((e.wheelDelta * 0.35) || (e.detail * -6.0));
+            this._scrollY = Math.max(0,Math.min(this._scrollY,this.scrollMax));
+            this._target.style.marginTop = -this._scrollY + 'px';
+            this._updateHandlePosition();
             e.preventDefault();
         };
 
-        //scoll position relative to handle
+        //scroll position relative to handle
         const setScrollFromHandle = (pageY,offset,rectTrack,rectHandle)=>{
             const min = 0;
             const max = rectTrack.height - rectHandle.height;
             const handle = Math.max(min,Math.min(pageY - rectTrack.top - offset,max));
 
             //update scroll container position
-            this._state.scrollY = (handle / max) * (this._target.offsetHeight - this._element.offsetHeight);
-            this._target.style.marginTop = -this._state.scrollY + 'px';
+            this._scrollY = (handle / max) * (this._target.offsetHeight - this._element.offsetHeight);
+            this._target.style.marginTop = -this._scrollY + 'px';
 
             //update scroll handle position
             this._elementHandle.style.marginTop = handle + 'px';
         };
 
-        //handle track click
-        const onTrackMouseDown = (e)=>{
-            const rectTrack = this._elementTrack.getBoundingClientRect();
-            const rectHandle = this._elementHandle.getBoundingClientRect();
-            setScrollFromHandle(e.pageY,rectHandle.height * 0.5,rectTrack,rectHandle);
-        };
-
-        //handle drag
         let rectTrack = null;
         let rectHandle = null;
         let offsetHandle = -1;
+
+        //handle track click
+        const onTrackMouseDown = (e)=>{
+            rectTrack = this._elementTrack.getBoundingClientRect();
+            rectHandle = this._elementHandle.getBoundingClientRect();
+            const min = e.pageY - rectTrack.top;
+            const max = rectTrack.bottom - e.pageY;
+            offsetHandle = rectHandle.height * 0.5;
+            //offset calc track top / bottom range
+            offsetHandle = offsetHandle > min ? min : //0 -> height / 2
+                           offsetHandle > max ? rectHandle.height - max : //height / 2 -> height
+                           offsetHandle;
+            setScrollFromHandle(e.pageY,offsetHandle,rectTrack,rectHandle);
+            this._trackDragging = true;
+        };
+
+        //handle drag
         const onHandleMouseDown = (e)=>{
             rectTrack = this._elementTrack.getBoundingClientRect();
             rectHandle = this._elementHandle.getBoundingClientRect();
             offsetHandle = e.pageY - rectHandle.top;
-            this._state.handleDragging = true;
+            this._trackDragging = false;
+            this._handleDragging = true;
+            e.stopPropagation();
         };
         const onHandleMouseMove = (e)=>{
-            if(!this._state.handleDragging){
+            if(!(this._handleDragging || this._trackDragging)){
                 return;
             }
             setScrollFromHandle(e.pageY,offsetHandle,rectTrack,rectHandle);
         };
         const onHandleMouseUp = ()=>{
-            this._state.handleDragging = false;
+            this._handleDragging = false;
+            this._trackDragging = false;
         };
 
         this._elementContainer.addEventListener('mousewheel',onContainerScroll);
@@ -120,56 +111,105 @@ export default class ScrollContainer{
         }
     }
 
-    _removeEventListeners(){}
-
-    _resetScroll(){
-        this._state.handleOffset = 0;
-        this._state.scrollY = 0;
-        this._target.style.marginTop = null;
-        this._elementHandle.style.marginTop = null;
-    }
-
+    /**
+     * Updates the handle height relative to the content height.
+     * @private
+     */
     _updateHandleHeight(){
         const heightTrack = this._elementTrack.offsetHeight;
-        const heightContent = this._target.offsetHeight;
-        const ratio = heightTrack / heightContent;
-        this._state.handleHeight = ratio * heightTrack;
-
-        //reset position container + handle position
-        this._elementHandle.style.height = this._state.handleHeight + 'px';
+        const ratio = heightTrack / this._target.offsetHeight;
+        this._elementHandle.style.height = (ratio * heightTrack) + 'px';
     }
 
-    setHeight(value,reset){
-        //not active
-        if(value === null){
-            //remove
-            if(this._state.height !== null){
+    /**
+     * Updates the handle position relative to the scroll offset.
+     * @private
+     */
+    _updateHandlePosition(){
+        const heightTrack = this._elementTrack.offsetHeight;
+        const max = this._target.offsetHeight - this._element.offsetHeight;
+        const ratio = this._elementHandle.offsetHeight / heightTrack;
+        this._elementHandle.style.marginTop = this._scrollY / max * (1.0 - ratio) * heightTrack + 'px';
+    }
+
+    /**
+     * Scrolls the target element to top.
+     */
+    scrollToTop(){
+        this.scrollTo(0);
+    }
+
+    /**
+     * Scrolls the target element to bottom.
+     */
+    scrollToBottom(){
+        this.scrollTo(this.scrollMax);
+    }
+
+    /**
+     * Scrolls the target element to a specified position.
+     * @param position
+     */
+    scrollTo(position){
+        this._scrollY = Math.max(0,Math.min(position,this.scrollMax));
+        this._target.style.marginTop = -this._scrollY + 'px';
+
+        this._updateHandleHeight();
+        this._updateHandlePosition();
+    }
+
+    /**
+     * Returns the maximum scroll position.
+     * @return {number}
+     */
+    get scrollMax(){
+        return this._target.offsetHeight - this._element.offsetHeight;
+    }
+
+    /**
+     * Sets the scroll-container height. If null the scroll-container gets removed.
+     * @param height
+     */
+    setHeight(height){
+        //scroll-container remove
+        if(height === null){
+            if(this._height !== null){
                 this._parent.removeChild(this._element);
                 this._parent.appendChild(this._target);
             }
-            this._resetScroll();
-            //active
+            this.scrollToTop();
+        //scroll-container active
         } else {
+            const heightTarget = this._target.offsetHeight;
+            height = Math.min(height,heightTarget);
             //update
-            if(this._state.height !== null){
-                // if(this._target.offsetHeight - this._state.scrollY <= value){
-                //     return;
-                // }
-                this._element.style.height = value + 'px';
-                //add
+            if(this._height !== null){
+                this._element.style.height = height + 'px';
+                //target offsetted
+                const diff = heightTarget - this._scrollY;
+                if(diff < height){
+                    this.scrollToBottom();
+                } else {
+                    this._updateHandleHeight();
+                    this._updateHandlePosition();
+                }
+            //scroll-container add
             } else {
                 this._elementContainer.appendChild(this._target);
                 this._parent.appendChild(this._element);
-                this._element.style.height = value + 'px';
-                this._resetScroll();
+                this._element.style.height = height + 'px';
+                this.scrollToTop();
             }
-            this._updateHandleHeight();
         }
-        this._state.height = value;
+        this._height = height;
     }
 
+    /**
+     * Returns the container´s current height.
+     * @return {null|*|number}
+     */
     get height(){
-        return this._state.height;
+        return this._height;
     }
 
     getContentBoundingClientRect(){
@@ -186,7 +226,12 @@ export default class ScrollContainer{
         };
     }
 
+    /**
+     * Detaches the scroll-container from the target element and
+     * removes all event listeners.
+     */
     clear(){
+        this.setHeight(null);
         this._removeEventListeners();
     }
 }
