b334c29121e143d2af1867887473fa74b765c6ec
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-11 17:56:03 +0200

    test api panel-height-set initial

---
+++ test/api/panel-height-set.js
@@ -0,0 +1,20 @@
+import ControlKit from '../../index';
+
+window.addEventListener('load',()=>{
+    const settings = {number:0};
+
+    new ControlKit().addPanel({height:300})
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number')
+        .addNumber(settings,'number');
+});
