930412378064e5cf7c75365595e6f68c67119177
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-11 17:54:19 +0200

    src Number added formatting, docs initial

--- src/component/Number.js
+++ src/component/Number.js
@@ -5,19 +5,42 @@ import createHtml from '../util/createHtml';
 import ObjectComponent from './ObjectComponent';
 import ComponentPreset from './ComponentPreset';
 
-const template = '<input type="number" value="1">';
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Template / Defaults
+/*--------------------------------------------------------------------------------------------------------------------*/
 
+const template = '<input type="number">';
+
+/**
+ * Number default config
+ * @type {Object}
+ * @property {string} label - The component label
+ * @property {boolean} readonly - If true, the component is readonly
+ * @property {number} preset - A set of preset values to choose from
+ * @property {number} min - Value min value
+ * @property {number} max - Value max value
+ * @property {number} fd - Number fo fractional digits displayed.
+ * @property {number} step - The amount stepped when pressing up/down keys.
+ * @property {number} stepShiftMult - The amount the stepping value gets multiplied with when holding shift while stepping.
+ * @property {function} onChange - Callback on value change.
+ * @property {null} annotation - A component annotation.
+ */
 export const DefaultConfig = Object.freeze({
     label : null,
     readonly : false,
     preset : null,
     min : null,
     max : null,
-    dp : null,
-    step : null,
+    fd : 4,
+    step : 0.25,
+    stepShiftMult: 2,
     onChange : function(){},
     annotation : null
 });
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Number
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 export default class Number_ extends ObjectComponent{
     /**
@@ -42,27 +65,77 @@ export default class Number_ extends ObjectComponent{
         this._state.readonly = config.readonly;
         this._state.min = config.min;
         this._state.max = config.max;
-        this._state.dp = config.dp;
+        this._state.fd = config.fd;
         this._state.step = config.step;
+        this._state.stepShiftMult = config.stepShiftMult;
         this._state.preset = config.preset;
 
         //elements
         this._element.classList.add('type-input');
-
-        const step = this._state.step;
-        const dp = this._state.dp;
         this._elementInput = this._elementWrap.appendChild(createHtml(template));
-        this._elementInput.setAttribute('step',(step || 1.0).toFixed(dp || 4));
+        //input set step, min, max
+        this._elementInput.setAttribute('step',this._state.step.toFixed(this._state.fd));
+        this._elementInput.setAttribute('min',this._state.min);
+        this._elementInput.setAttribute('max',this._state.max);
+
+        const formatValueFromInput = (value)=>{
+            this.value = this._elementInput.value = this._formatValue(value);
+        };
+
         this._elementInput.addEventListener('input',()=>{
             if(this._state.readonly){
                 return;
             }
-            let value = this._elementInput.value;
-            value = +value;
-            value = Number.isNaN(value) ? (this._state.min || 0) : value;
-            value = dp !== null ? value.toFixed(dp) : value;
-            this.value = +value;
+            this.value = this._formatValue(this._elementInput.valueAsNumber);
         });
+        //input format on enter
+        this._elementInput.addEventListener('change',()=>{
+            formatValueFromInput(this._elementInput.value);
+        });
+        //input format on step
+        this._elementInput.addEventListener('keydown',(e)=>{
+            if(this._state.readonly){
+                return;
+            }
+            let step = this._state.step;
+
+            if(step != null){
+                //manual mult stepping
+                if(e.shiftKey){
+                    step *= this._state.stepShiftMult;
+                    switch(e.code){
+                        case 'ArrowUp':
+                            formatValueFromInput(this._elementInput.valueAsNumber + step);
+                            break;
+                        case 'ArrowDown':
+                            formatValueFromInput(this._elementInput.valueAsNumber - step);
+                            break;
+                    }
+                }
+                //build-in step
+                else {
+                    switch(e.code){
+                        case 'ArrowUp':
+                        case 'ArrowDown':
+                            formatValueFromInput(this._elementInput.valueAsNumber);
+                            break;
+                    }
+                }
+            } else {
+                //prevent stepping
+                switch(e.code){
+                    case 'ArrowUp':
+                    case 'ArrowDown':
+                        e.preventDefault();
+                        break;
+                }
+            }
+        });
+
+        //input prevent mouse wheel stepping
+        const onMouseWheel = (e)=>{e.preventDefault();};
+        this._elementInput.addEventListener('mousewheel',onMouseWheel);
+        this._elementInput.addEventListener('wheel',onMouseWheel);
 
         //preset selection
         this._preset = new ComponentPreset(this._elementInput);
@@ -78,6 +151,12 @@ export default class Number_ extends ObjectComponent{
         this.sync();
     }
 
+    /**
+     * Sets the min or max value.
+     * @param key
+     * @param value
+     * @private
+     */
     _setMinMax(key,value){
         this._state[key] = value;
         if(value === null){
@@ -85,6 +164,31 @@ export default class Number_ extends ObjectComponent{
             return;
         }
         this._elementInput.setAttribute(key,value);
+        this.sync();
+    }
+
+    /**
+     * Formats a value based on min / max constrains and fractional digits allowed.
+     * @param x
+     * @return {*}
+     * @private
+     */
+    _formatValue(x){
+        const fd  = this._state.fd;
+        const min = this._state.min;
+        const max = this._state.max;
+        x = +x;
+        if(min != null && max != null){
+            x = Number.isNaN(x) ? min : Math.max(min,Math.min(x,max));
+        } else if(min != null){
+            x = Number.isNaN(x) ? min : Math.max(min,x);
+        } else if(max != null){
+            x = Number.isNaN(x) ? max : Math.min(x,max);
+        } else {
+            x = Number.isNaN(x) ? 0 : x;
+        }
+        x = fd != null ? x.toFixed(fd) : x;
+        return x;
     }
 
     /**
@@ -173,6 +277,6 @@ export default class Number_ extends ObjectComponent{
      * got changed externally.
      */
     sync(){
-        this._elementInput.value = this.value;
+        this._elementInput.value = this._formatValue(this.value);
     }
 }
