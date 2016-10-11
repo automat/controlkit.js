bc7a7b8609a7f163f400ccd96672cbd6e03be53f
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-11 17:57:46 +0200

    test api component-number initial

---
+++ test/api/component-number.js
@@ -0,0 +1,12 @@
+import ControlKit from '../../index';
+
+window.addEventListener('load',()=>{
+    const settings = {a:0,b:1,c:1.001,d:1.0001};
+
+    new ControlKit().addPanel()
+        .addNumber(settings,'a',{readonly : true})
+        .addNumber(settings,'a',{fd:10,onChange:value=>console.log(value)})
+        .addNumber(settings,'b',{fd:1,onChange:function(){console.log(this.value);}})
+        .addNumber(settings,'c',{fd:0,stepShiftMult:10,step:1})
+        .addNumber(settings,'d')
+});
