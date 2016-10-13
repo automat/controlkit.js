51fc7dc47329400fdb964d6ed1991aab55e43383
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-13 23:44:27 +0200

    src Label DefaultConfig stub

--- src/component/Label.js
+++ src/component/Label.js
@@ -1,5 +1,7 @@
 import Component from './Component';
 
+export const DefaultConfig = {};
+
 export default class Label extends Component{
     constructor(parent,label){
         super(parent,{label});
