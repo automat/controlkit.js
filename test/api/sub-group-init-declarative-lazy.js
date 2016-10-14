d48964b77a0392056a374c9f989768a858c34cdd
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-14 10:41:30 +0200

    test api sub-group-init-declarative-lazy initial

---
+++ test/api/sub-group-init-declarative-lazy.js
@@ -0,0 +1,22 @@
+import ControlKit from '../../index';
+
+window.addEventListener('load',()=>{
+    const settings = {number:0};
+
+    const controlKit = new ControlKit();
+    controlKit.add({
+        subGroups : [{
+            label : 'sub group a',
+            components:[
+                {type:'number',object:settings,key:'number'},
+                {type:'number',object:settings,key:'number'}
+            ]
+        },{
+            label : 'sub group b',
+            components:[
+                {type:'number',object:settings,key:'number'},
+                {type:'number',object:settings,key:'number'}
+            ]
+        }]
+    });
+});
