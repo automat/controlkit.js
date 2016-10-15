a1e7da41c47a390f877e449a7e997951c7cc2b1d
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-15 18:57:46 +0200

    src ComponentPreset added ComponentOptions toggle

--- src/component/ComponentPreset.js
+++ src/component/ComponentPreset.js
@@ -2,14 +2,23 @@ import EventEmitter from 'events';
 import createHtml from '../util/create-html';
 import ComponentOptions from './ComponentOptions';
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Template
+/*--------------------------------------------------------------------------------------------------------------------*/
+
 const templatePresetWrap = `<div class="input-wrap-preset"></div>`;
 const templatePresetButton = `<button class="btn-preset"></button>`;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Component preset
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 export default class ComponentPreset extends EventEmitter{
     constructor(input){
         super();
         this.setMaxListeners(0);
 
+        //options ref
         this._options = null;
 
         //elements
@@ -18,30 +27,45 @@ export default class ComponentPreset extends EventEmitter{
         this._elementWrap = createHtml(templatePresetWrap);
         this._elementButton = createHtml(templatePresetButton);
 
-        //listener
+        //options
         const options = ComponentOptions.sharedOptions();
-        this._elementButton.addEventListener('click',()=>{
-            options.dock(this._input);
-            options.options = this._options;
-            options.enable = true;
-            // this._elementButton.classList.add()
-        });
-        options.on('change',(info)=>{
-            if(info.input !== this._input){
+        options.on('change',(target,value)=>{
+            if(target !== this._input){
                 return;
             }
-            this.emit('change',info.value);
+            this.emit('change',value);
+        });
+        this._elementButton.addEventListener('click',()=>{
+            //remove
+            if(options.enable && options.target === this._input){
+                options.enable = false;
+                return;
+            }
+            //add
+            options.target = this._input;
+            options.options = this._options;
         });
     }
 
+    /**
+     * Sets a list of preset option. If null the preset toggle get removed.
+     * @param value
+     */
     set options(value){
+        if(!(value instanceof Array)){
+            throw new Error('Options not of type "Array".');
+        }
+
+        //add
         if(value){
             this._parent.removeChild(this._input);
             this._parent.appendChild(this._elementWrap);
             this._parent.appendChild(this._elementButton);
             this._elementWrap.appendChild(this._input);
             this._options = value.slice(0);
-        } else {
+        }
+        //remove
+        else {
             if(this._parent.contains(this._elementWrap)){
                 this._parent.removeChild(this._elementWrap);
                 this._parent.removeChild(this._elementButton);
@@ -51,6 +75,10 @@ export default class ComponentPreset extends EventEmitter{
         }
     }
 
+    /**
+     * Returns the optons set
+     * @return {*}
+     */
     get options(){
         return this._options ? this._options.slice(0) : null;
     }
