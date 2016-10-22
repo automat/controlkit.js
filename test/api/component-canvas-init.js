5632b85491018615c917a2fa2a2e9ab2bf7385fe
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-22 17:52:51 +0200

    test api component-canvas-init initial

---
+++ test/api/component-canvas-init.js
@@ -0,0 +1,37 @@
+import ControlKit from '../../index';
+
+window.addEventListener('load',()=>{
+
+    new ControlKit().addPanel()
+        .addCanvas({
+            init(){
+                this.getRandomColor = ()=>{
+                    return `rgb(
+                        ${Math.floor(Math.random() * 255)},
+                        ${Math.floor(Math.random() * 255)},
+                        ${Math.floor(Math.random() * 255)}
+                    )`;
+                };
+                this.color = this.getRandomColor();
+            },
+            onMouseDown(){
+                this.color = this.getRandomColor();
+            },
+            onMouseDrag(e){
+                const radius = 5;
+                this.ctx.save();
+                this.ctx.translate(e.x - radius,e.y);
+                this.ctx.fillStyle = this.color;
+                this.ctx.beginPath();
+                this.ctx.moveTo(radius,0);
+                this.ctx.arc(radius,0,radius,0,Math.PI * 2);
+                this.ctx.fill();
+                this.ctx.restore();
+            },
+            onKeyPress(e){
+                if(e.key == ' '){
+                    this.ctx.clearRect(0,0,this.width,this.height);
+                }
+            }
+        });
+});
