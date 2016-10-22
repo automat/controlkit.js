74ab2888099e91299a72874030855400b267f423
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-22 12:35:42 +0200

    test api component-pad-init initial

---
+++ test/api/component-pad-init.js
@@ -0,0 +1,10 @@
+import ControlKit from '../../index';
+
+window.addEventListener('load',()=>{
+    const settings = {a:[0,0],b:0};
+    const range = [[-2,1],[0,1]];
+
+    new ControlKit().addPanel()
+        .addPad(settings,'a',{rangeX: range[0],rangeY:range[1]})
+        .addPad(settings,'a',{label:'none'})
+});
