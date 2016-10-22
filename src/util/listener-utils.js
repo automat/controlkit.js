63c5e227f9a3c501678710278f94052314abb575
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-22 17:51:27 +0200

    src listener-utils initial

---
+++ src/util/listener-utils.js
@@ -0,0 +1,74 @@
+const noop = ()=>{};
+
+/**
+ * Attaches mouse listeners to an HTMLElement. Extends mouse drags events to the document level so they
+ * can be registered while holding and dragging outside the element. Returns a function to remove the document attached listeners.
+ * @param {HTMLElement} element
+ * @param config
+ * @return {Function}
+ */
+export function attachMouseListenersDocumentExtended(element,config){
+    config = config || {};
+    config.onMouseDown = config.onMouseDown;
+    config.onMouseMove = config.onMouseMove;
+    config.onMouseUp = config.onMouseUp || noop;
+    config.onMouseDrag = config.onMouseDrag || noop;
+    config.args = config.args || [];
+
+    let rect = null;
+    let dragging = false;
+
+    //extend event with local position
+    const attach = (e)=>{
+        const e_ = {};
+        for(const key in e){
+            e_[key] = e[key];
+        }
+        e_.x = e.pageX - rect.left;
+        e_.y = e.pageY - rect.top;
+        return e_;
+    };
+
+    //mouse down
+    element.addEventListener('mousedown',(e)=>{
+        rect = element.getBoundingClientRect();
+        config.onMouseDown.apply(null,[attach(e)].concat(config.args));
+        dragging = true;
+    });
+
+    //mouse move
+    element.addEventListener('mousemove',(e)=>{
+        if(dragging){
+            return;
+        }
+        rect = element.getBoundingClientRect();
+        config.onMouseMove.apply(null,[attach(e)].concat(config.args));
+    });
+
+    //mouse move on drag
+    const onDocumentMouseMove = (e)=>{
+        if(!dragging){
+            return;
+        }
+        config.onMouseDrag.apply(null,[attach(e)].concat(config.args));
+    };
+
+    //mouse up
+    const onDocumentMouseUp = (e)=>{
+        if(!dragging){
+            return;
+        }
+        config.onMouseUp([attach(e)].concat(config.args));
+        dragging = false;
+    };
+
+    //attach document
+    document.addEventListener('mousemove',onDocumentMouseMove);
+    document.addEventListener('mouseup',onDocumentMouseUp);
+
+    //detach document
+    return function(){
+        document.removeEventListener('mousemove',onDocumentMouseMove);
+        document.removeEventListener('mouseup',onDocumentMouseUp);
+    }
+}
