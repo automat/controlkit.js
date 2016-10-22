6f48927d728a9cda359e5f477edea9a14edd42d0
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-22 12:33:29 +0200

    src MathUtils added map

--- src/util/MathUtils.js
+++ src/util/MathUtils.js
@@ -2,6 +2,10 @@ export function normalize(a,start,end){
     return (a - start) / (end - start);
 }
 
+export function map(a,inMin,inMax,outMin,outMax){
+    return outMin + (outMax - outMin) * (a - inMin) / (inMax - inMin);
+}
+
 export function clamp(x,min,max){
     return Math.max(min,Math.min(x,max));
 }
