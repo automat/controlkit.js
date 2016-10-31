998ed94a0e3fcf7ee1ceb9c22b0892336a781e65
Henryk Wollik <olafyoorsen@hotmail.com>
2016-10-31 20:36:59 +0100

    src FunctionPlotter added x shift drag

--- src/component/FunctionPlotter.js
+++ src/component/FunctionPlotter.js
@@ -1,6 +1,7 @@
 import validateOption from 'validate-option';
 import validateType from '../util/validate-type';
 import ObjectComponent from './ObjectComponent';
+import {normalize,map} from '../util/math-utils';
 import {attachMouseListenersDocumentExtended} from '../util/listener-utils';
 
 const noop = ()=>{};
@@ -28,8 +29,8 @@ export const DefaultConfig = Object.freeze({
     label : null,
     rangeX : [-1,1],
     rangeY : [-1,1],
-    steps : 200,
-    useFill: false,
+    steps : 300,
+    useFill: true,
     fill: null,
     stroke: null
 });
@@ -53,6 +54,7 @@ export default class FunctionPlotter extends ObjectComponent{
         this._state.rangeY = config.rangeY;
         this._state.steps = config.steps;
         this._state.useFill = config.useFill;
+        this._state.offset = [0,0];
 
         this._elementSvg = this._element.querySelector('svg');
         this._group = this._elementSvg.querySelector('g');
@@ -61,15 +63,37 @@ export default class FunctionPlotter extends ObjectComponent{
         this._graph.style.stroke = this._state.stroke || null;
         this._graph.style.fill = this._state.useFill ? null : 'none';
 
+        let offsetDown = [0,0];
+        let offsetUp = [0,0];
         this._removeEventListeners = attachMouseListenersDocumentExtended(this._elementSvg,{
             onMouseDown : (e)=>{
-
+                offsetDown[0] = e.x / e.rect.width;
+                offsetDown[1] = e.y / e.rect.height;
             },
             onMouseDrag : (e)=>{
+                //const rect = e.rect;
+                const ox = e.x / e.rect.width - offsetDown[0];
+                const oy = e.y / e.rect.height - offsetDown[1];
+                // const oxn = ox / rect.width;
+                // const oyn = oy / rect.height;
 
+                const rangeX = this._state.rangeX;
+                const rangeY = this._state.rangeY;
+                const spanX = Math.abs(rangeX[0] - rangeX[1]);
+                const spanY = Math.abs(rangeY[0] - rangeY[1]);
+                const oxs = ox * spanX;
+                const oys = oy * spanY;
+
+                this._state.offset[0] = offsetUp[0] + oxs;
+                this._state.offset[1] = offsetUp[1] + oys;
+
+                console.log(this._state.offset[0]);
+
+                this.sync();
             },
             onMouseUp : (e)=>{
-
+                offsetUp[0] = this._state.offset[0];
+                offsetUp[1] = this._state.offset[1];
             }
         });
 
@@ -109,10 +133,14 @@ export default class FunctionPlotter extends ObjectComponent{
         const ymin = rangeY[0];
         const ymax = rangeY[1];
 
+        //offset drag
+        const offsetX = this._state.offset[0];
+        const offsetY = this._state.offset[1];
+
         //relative axes position
         const oxr = -xmin / (xmax - xmin);
         const oyr = 1.0 - (-ymin / (ymax - ymin));
-        const axx = Math.max(1,Math.min(width * oxr,width - 1));
+        const axx = Math.max(1,Math.min(width * oxr + offsetX,width - 1));
         const ayy = Math.max(1,Math.min(height * oyr,height - 1));
 
         //y over x
@@ -120,7 +148,7 @@ export default class FunctionPlotter extends ObjectComponent{
         for(let i = 0; i < steps; ++i){
             const n = i / (steps - 1);
             const x = xmin + n * (xmax - xmin);
-            const y = 1.0 - func(x);
+            const y = 1.0 - normalize(func(x - offsetX),ymin,ymax);
             const ax = n * width;
             const ay = y * height;
             points.push(ax,ay);
