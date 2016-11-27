4afb7265c7a2ea418b264785ca8f4280bc3214bd
Henryk Wollik <olafyoorsen@hotmail.com>
2016-11-27 10:44:51 +0100

    test api component-function-plotter-init added additional setup args

--- test/api/component-function-plotter-init.js
+++ test/api/component-function-plotter-init.js
@@ -1,9 +1,12 @@
 import ControlKit from '../../index';
 
 window.addEventListener('load',()=>{
-    const settings = {func:(x)=>{return (0.5 + Math.sin(Math.PI * 0.5 + x * Math.PI * 4) * 0.5);}};
+    const settings = {
+        func0:(x)=>{return (0.5 + Math.sin(Math.PI * 0.5 + x * Math.PI * 4) * 0.5);},
+        func1:(x)=>{return x;}
+    };
 
     new ControlKit().addPanel()
-        .addFunctionPlotter(settings,'func')
-        .addFunctionPlotter(settings,'func',{stroke:'#ff0'});
+        .addFunctionPlotter(settings,'func0',{useFill:false,scaleX:true,dragX:true,dragY:true})
+        .addFunctionPlotter(settings,'func1',{stroke:'#fff',strokeWidth:3,useFill:false,scaleX:true});
 });
