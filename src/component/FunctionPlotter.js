6ff44c24472f783846961a0d5d02af0343efb10a
Henryk Wollik <olafyoorsen@hotmail.com>
2016-11-27 10:44:20 +0100

    src FunctionPlotted added drag{X,Y}, scaleX

--- src/component/FunctionPlotter.js
+++ src/component/FunctionPlotter.js
@@ -1,10 +1,8 @@
 import validateOption from 'validate-option';
 import validateType from '../util/validate-type';
 import ObjectComponent from './ObjectComponent';
-import {normalize,map} from '../util/math-utils';
+import {normalize} from '../util/math-utils';
 import {attachMouseListenersDocumentExtended} from '../util/listener-utils';
-
-const noop = ()=>{};
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Template / Defaults
@@ -24,6 +22,17 @@ const template =
  * Function plotter component default config.
  * @type {Object}
  * @property {string} label - The component label
+ * @property {number[]} rangeX - x range
+ * @property {number[]} rangeY - y range
+ * @property {number} steps - Point render steps
+ * @property {boolean} useFill - If true, the graph is closed and filled.
+ * @property {string} fill - Graph fill color
+ * @property {string} stroke - Graph stroke color
+ * @property {number} strokeWidth - Graph stroke width
+ * @property {boolean} dragX - If true, graph can be dragged on x-axis.
+ * @property {boolean} dragY - If true, graph can be dragged on y-axis.
+ * @property {boolean} scaleX - If true, graph can be scaled on x-axis.
+ * @property {number} scaleXStep - X-axis scale step.
  */
 export const DefaultConfig = Object.freeze({
     label : null,
@@ -32,7 +41,12 @@ export const DefaultConfig = Object.freeze({
     steps : 300,
     useFill: true,
     fill: null,
-    stroke: null
+    stroke: null,
+    strokeWidth : null,
+    dragX : true,
+    dragY : false,
+    scaleX : false,
+    scaleXStep : 0.125
 });
 
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -49,54 +63,82 @@ export default class FunctionPlotter extends ObjectComponent{
             template
         });
 
+        //state
         this._state.padding = config.padding;
         this._state.rangeX = config.rangeX;
         this._state.rangeY = config.rangeY;
         this._state.steps = config.steps;
         this._state.useFill = config.useFill;
+        this._state.fill = config.fill;
+        this._state.stroke = config.stroke;
+        this._state.strokeWidth = config.strokeWidth;
+        this._state.dragX = config.dragX;
+        this._state.dragY = config.dragY;
+        this._state.scaleX = config.scaleX;
+        this._state.scaleXStep = config.scaleXStep;
+        this._state.scaleXSum = 1;
         this._state.offset = [0,0];
 
+        //elements
         this._elementSvg = this._element.querySelector('svg');
         this._group = this._elementSvg.querySelector('g');
         this._axes = this._group.querySelector('.axes');
         this._graph = this._group.querySelector('.function-graph');
         this._graph.style.stroke = this._state.stroke || null;
+        this._graph.style.strokeWidth = this._state.strokeWidth || null;
         this._graph.style.fill = this._state.useFill ? null : 'none';
 
+        //mouse drag + zoom
+        let down = false;
         let offsetDown = [0,0];
         let offsetUp = [0,0];
         this._removeEventListeners = attachMouseListenersDocumentExtended(this._elementSvg,{
             onMouseDown : (e)=>{
+                if(!this._state.dragX && !this._state.dragY){
+                    return;
+                }
                 offsetDown[0] = e.x / e.rect.width;
                 offsetDown[1] = e.y / e.rect.height;
+                down = true;
             },
             onMouseDrag : (e)=>{
-                //const rect = e.rect;
+                if(!this._state.dragX && !this._state.dragY){
+                    return;
+                }
                 const ox = e.x / e.rect.width - offsetDown[0];
                 const oy = e.y / e.rect.height - offsetDown[1];
-                // const oxn = ox / rect.width;
-                // const oyn = oy / rect.height;
 
-                const rangeX = this._state.rangeX;
-                const rangeY = this._state.rangeY;
-                const spanX = Math.abs(rangeX[0] - rangeX[1]);
-                const spanY = Math.abs(rangeY[0] - rangeY[1]);
-                const oxs = ox * spanX;
-                const oys = oy * spanY;
-
-                this._state.offset[0] = offsetUp[0] + oxs;
-                this._state.offset[1] = offsetUp[1] + oys;
-
-                console.log(this._state.offset[0]);
+                this._state.offset[0] = offsetUp[0] + ox;
+                this._state.offset[1] = offsetUp[1] + oy;
 
                 this.sync();
             },
-            onMouseUp : (e)=>{
+            onMouseWheel : (e)=>{
+                if(!this._state.scaleX){
+                    return;
+                }
+                const delta = e.wheelDelta | e.detail;
+                const scale = delta < 0 ? -1 : delta > 0 ? 1 : 0;
+                this._state.scaleXSum += scale * this._state.scaleXStep;
+                this._state.scaleXSum = Math.max(this._state.scaleXStep,this._state.scaleXSum);
+
+                this.sync();
+
+                //prevent container scroll
+                e.preventDefault();
+                e.stopPropagation();
+            },
+            onMouseUp : ()=>{
+                if(!this._state.dragX && !this._state.dragY){
+                    return;
+                }
                 offsetUp[0] = this._state.offset[0];
                 offsetUp[1] = this._state.offset[1];
+                down = false;
             }
         });
 
+        //resize
         const resize = ()=>{
             this._elementSvg.style.width = null;
             const size = this._elementSvg.getBoundingClientRect().width;
@@ -132,38 +174,46 @@ export default class FunctionPlotter extends ObjectComponent{
         const xmax = rangeX[1];
         const ymin = rangeY[0];
         const ymax = rangeY[1];
+        const spanX = Math.abs(xmin - xmax);
+        const spanY = Math.abs(ymin - ymax);
 
         //offset drag
-        const offsetX = this._state.offset[0];
-        const offsetY = this._state.offset[1];
+        const scaleX = this._state.scaleXSum;
+        const offsetX = this._state.offset[0] * +this._state.dragX;
+        const offsetY = this._state.offset[1] * +this._state.dragY;
+        const offsetXFunc = offsetX * spanX * scaleX;
+        const offsetYFunc = offsetY * spanY;
 
         //relative axes position
         const oxr = -xmin / (xmax - xmin);
         const oyr = 1.0 - (-ymin / (ymax - ymin));
-        const axx = Math.max(1,Math.min(width * oxr + offsetX,width - 1));
-        const ayy = Math.max(1,Math.min(height * oyr,height - 1));
+        const axx = Math.max(1,Math.min(width * (oxr + offsetX),width - 1));
+        const ayy = Math.max(1,Math.min(height * (oyr + offsetY),height - 1));
 
         //y over x
         const points = [];
         for(let i = 0; i < steps; ++i){
             const n = i / (steps - 1);
             const x = xmin + n * (xmax - xmin);
-            const y = 1.0 - normalize(func(x - offsetX),ymin,ymax);
+            const y = 1.0 - (normalize(func(x * scaleX - offsetXFunc) - offsetYFunc,ymin,ymax));
             const ax = n * width;
             const ay = y * height;
             points.push(ax,ay);
         }
 
+        //optional close graph
         if(this._state.useFill){
             points.unshift(points[0],height);
             points.push(points[points.length-2],height);
         }
 
+        //create path cmd
         let path = `M ${points[0]} ${points[1]} `;
         for(let i = 2; i < points.length; i+=2){
             path += `L ${points[i]} ${points[i+1]} `;
         }
 
+        //set axes + graph path cmd
         this._axes.setAttribute('d',
             `M ${axx} ${0} L ${axx} ${height} 
              M ${0} ${ayy} L ${width} ${ayy}`
