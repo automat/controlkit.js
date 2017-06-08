d15e4615456d96ba5685611989cf7754112478e8
Henryk Wollik <olafyoorsen@hotmail.com>
2017-06-08 13:28:19 +0200

    test text-init initial

---
+++ test/api/component/text-init.js
@@ -0,0 +1,6 @@
+import ControlKit from '../../../index';
+
+window.addEventListener('load',()=>{
+    new ControlKit().addPanel()
+        .addText('Title','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend egestas dui, id facilisis urna lobortis eget. Curabitur commodo neque vitae libero condimentum, sed egestas urna euismod. Proin justo lorem, tristique nec ligula in, pharetra venenatis mauris. \nSuspendisse potenti. Duis iaculis enim quam, quis congue elit imperdiet iaculis');
+});
