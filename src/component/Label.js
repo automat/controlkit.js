e770a3e37b73856528c435539fa5947cb1072497
Henryk Wollik <olafyoorsen@hotmail.com>
2017-06-10 17:06:28 +0200

    Label doc++

--- src/component/Label.js
+++ src/component/Label.js
@@ -1,12 +1,34 @@
 import validateOption from 'validate-option';
 import Component from './Component';
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Defaults
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * Label default config
+ * @type {object}
+ * @property {string|null} id
+ * @property {string|null} label
+ */
 export const DefaultConfig = {
     id : null,
     label : null
 };
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Label
+/*--------------------------------------------------------------------------------------------------------------------*/
+
 export default class Label extends Component{
+    /**
+     * @constructor
+     * @param parent
+     * @param {string} label
+     * @param {object} [config]
+     * @param {string|null} [config.label]
+     * @param {string|null} [config.id]
+     */
     constructor(parent,label,config){
         config = validateOption(config, DefaultConfig);
 
