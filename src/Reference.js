f3b41898a51bfa4cf881d4f78d4b351a42f5f749
Henryk Wollik <olafyoorsen@hotmail.com>
2017-10-01 19:35:04 +0200

    reference refactor

--- src/Reference.js
+++ src/Reference.js
@@ -1,48 +1,81 @@
-const map = new Map();
+export default class Reference{
+    constructor(){
+        this._map = new Map();
+    }
 
-const Reference = {
-    set : function(id,object){
-        const hasId = map.has(id);
-        const hasIdObj = map.has(object.id);
+    /**
+     * Creates object by id ref.
+     * @param id
+     * @param obj
+     */
+    set(id,obj){
+        const hasId = this._map.has(id);
+        const hasIdObj =  this._map.has(obj.id);
         //invalid
-        if(id == null && object.id == null){
+        if(id === null && obj.id === null){
             throw new Error(`Invalid id "${id}".`);
         }
         //remove obj
-        if(id == null && hasIdObj){
-            map.delete(object.id);
+        if(id === null && hasIdObj){
+            this._map.delete(obj.id);
             return
         }
         //obj referenced already
-        if(id == object.id && hasId){
+        if(id === obj.id && hasId){
             return;
         }
         //remove duplicate ids
         if(hasId && hasIdObj){
-            map.delete(id);
-            map.delete(object.id);
+            this._map.delete(id);
+            this._map.delete(obj.id);
         }
 
-        map.set(id,object);
-    },
-    has : function(id){
-        return map.has(id);
-    },
-    get : function(id){
-        if(!map.has(id)){
-            throw new Error(`Invalid id "${id}".`)
-        }
-        return map.get(id);
-    },
-    delete : function(id){
-        if(!map.has(id)){
-            throw new Error(`Invalid id "${id}".`)
-        }
-        map.delete(id);
-    },
-    clear : function(){
-        map.clear();
+        this._map.set(id,obj);
     }
-};
 
-export default Reference;
+    /**
+     * Returns object by id.
+     * @param id_or_ids
+     * @return {Array}
+     */
+    get(id_or_ids){
+        if(id_or_ids instanceof Array){
+            const elements = new Array(id_or_ids.length);
+            for(let i = 0; i < id_or_ids.length; ++i){
+                elements[i] = this.get(id_or_ids[i]);
+            }
+            return elements;
+        }
+        if(!this.has(id_or_ids)){
+            throw new Error(`Invalid id "${id_or_ids}".`)
+        }
+        return this._map.get(id_or_ids);
+    }
+
+    /**
+     * Returns true if object referenced.
+     * @param id
+     * @return {boolean}
+     */
+    has(id){
+        return this._map.has(id);
+    }
+
+    /**
+     * Removes id object reference.
+     * @param id
+     */
+    delete(id){
+        if(!this._map.has(id)){
+            throw new Error(`Invalid id "${id}".`)
+        }
+        this._map.delete(id);
+    }
+
+    /**
+     * Completely clears all id object references
+     */
+    clear(){
+        this._map.clear();
+    }
+}
