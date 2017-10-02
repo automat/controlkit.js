d89c2030167b4ce8fd0601294b9700a1a8b7651b
Henryk Wollik <olafyoorsen@hotmail.com>
2017-10-02 08:57:39 +0200

    test group-height initial

--- test/api/group/group-height.js
+++ test/api/group/group-height.js
@@ -1,9 +1,9 @@
-import ControlKit from '../../index';
+import ControlKit from '../../../index';
 
 window.addEventListener('load',()=>{
     const settings = {number:0};
 
-    new ControlKit().addPanel({height:400})
+    new ControlKit().addPanel()
         .addGroup({height:300})
         .addNumber(settings,'number')
         .addNumber(settings,'number')
