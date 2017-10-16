dc1cf5ab9e5162d418067ac6a2539a701d9669e2
Henryk Wollik <olafyoorsen@hotmail.com>
2017-10-16 11:02:20 +0200

    test sub-group-height update api

--- test/api/group/sub-group-height.js
+++ test/api/group/sub-group-height.js
@@ -3,15 +3,18 @@ import ControlKit from '../../../index';
 window.addEventListener('load',()=>{
     const settings = {number:0};
 
-    new ControlKit().addPanel()
-        .addGroup({label:'group'})
-        .addSubGroup({label:'sub-group',height:100})
-        .add({type:'number',object:settings,key:'number'})
-        .add({type:'number',object:settings,key:'number'})
-        // .add({type:'number',object:settings,key:'number'})
-        // .add({type:'number',object:settings,key:'number'})
-        // .add({type:'number',object:settings,key:'number'})
-        // .add({type:'number',object:settings,key:'number'})
-        // .add({type:'number',object:settings,key:'number'})
-        // .add({type:'number',object:settings,key:'number'})
+    const controlKit = new ControlKit();
+    controlKit.add({
+        groups : [{
+            label : 'group',
+            groups : [{
+                label : 'sub-group',
+                height : 100,
+                comps : [
+                    {type:'number',object:settings,key:'number'},
+                    {type:'number',object:settings,key:'number'}
+                ]
+            }]
+        }]
+    })
 });
